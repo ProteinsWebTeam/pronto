dc00145e07224d80efc06e623a1481c285bd3ce5
Matthias Blum <mat.blum@gmail.com>
2017-12-05 17:56:05 +0000

    Fix internal link

--- static/app.js
+++ static/app.js
@@ -1595,7 +1595,7 @@ function ComparisonViews(methodsIds) {
                 html += method.isSelected ? '<tr class="selected">' : '<tr>';
 
                 if (method.entryId)
-                    html += '<td class="nowrap"><a href="'+ method.entryId +'">'+ method.entryId +'&nbsp;<i class="external icon"></i></a></td>';
+                    html += '<td class="nowrap"><a href="/entry/'+ method.entryId +'">'+ method.entryId +'&nbsp;<i class="external icon"></i></a></td>';
                 else
                     html += '<td></td>';
 
