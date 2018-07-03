9e5f2d8d9dd413fba2fbb4b55817297a155737be
Matthias Blum <mat.blum@gmail.com>
2018-07-03 15:17:14 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pronto',
+    version='1.2',
+    packages=['pronto'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'cx_Oracle>=5.3',
+        'Flask>=0.12.2',
+        'Jinja2>=2.10'
+    ],
+)
