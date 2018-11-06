b35524d7e693f46e6be338161b6bd600068f7260
Matthias Blum <mat.blum@gmail.com>
2018-11-06 20:00:59 +0000

    Updating project requirements, bumping version

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from setuptools import setup
 
 setup(
     name='pronto',
-    version='1.2.2',
+    version='1.3',
     packages=['pronto'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'cx_Oracle>=5.3',
         'Flask>=0.12.2',
-        'Jinja2>=2.10'
+        'Jinja2>=2.10',
+        'mysqlclient=>1.3'
     ],
 )
