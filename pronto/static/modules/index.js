8a5bd42b6ac7e408010a95f22c0e18400cee6f04
Matthias Blum <mat.blum@gmail.com>
2018-11-28 20:33:08 +0000

    Home page using API calls

--- pronto/static/modules/index.js
+++ pronto/static/modules/index.js
@@ -1,33 +1,23 @@
 import * as utils from '../utils.js';
 
+
 $(function () {
-    const feed = document.getElementById('feed');
-
-    if (feed) {
-        utils.getJSON('/api/feed/?n=15', (data, status) => {
+    fetch('/api/interpro/databases/')
+        .then(response => response.json())
+        .then(databases => {
             let html = '';
-            data.results.forEach(e => {
-                const date = new Date(e.timestamp * 1000);
-                const timeDelta = Math.floor(Date.now() / 1000) - e.timestamp;
-
-                html += '<div class="event">' +
-                    '<div class="content"><div class="date"><abbr title="'+ date.toLocaleString() +'">';
-
-                if (timeDelta < 60)
-                    html += timeDelta.toString() + 's';
-                else if (timeDelta < 3600)
-                    html += Math.floor(timeDelta / 60).toString() + 'm';
-                else if (timeDelta < (3600 * 24))
-                    html += Math.floor(timeDelta / 3600).toString() + 'h';
-                else
-                    html += date.toLocaleDateString();
-
-                html += '</abbr></div>';
-                html += '<div class="summary"><a class="user">'+ e.user +'</a> '+ e.event +'</div></div></div>';
+            databases.forEach(db => {
+                html += '<tr>'
+                    + '<td class="collapsing">'
+                    + '<a target="_blank" href="'+ db.home +'">'+ db.name +'&nbsp;<i class="external icon"></i></a>'
+                    + '</td>'
+                    + '<td><span class="ui basic label">'+ db.version +'<span class="detail">'+ db.date +'</span></span></td>'
+                    + '<td><a href="/database/' + db.short_name + '/">'+ db.count_signatures.toLocaleString() +'</a></td>'
+                    + '<td>'+ db.count_integrated.toLocaleString() +'</td>'
+                    + '<td><a href="/database/' + db.short_name + '/unintegrated/">'+ db.count_unintegrated.toLocaleString() +'</a></td>'
+                    + '</tr>';
             });
 
-            feed.innerHTML = html;
+            document.querySelector("table > tbody").innerHTML = html;
         });
-    }
-
 });
