f39b077829a7ab643807775b43d3be3dbdee008d
Matthias Blum <mat.blum@gmail.com>
2018-12-18 14:55:09 +0000

    More refactoring

--- pronto/static/modules/signatures/descriptions.js
+++ pronto/static/modules/signatures/descriptions.js
@@ -2,70 +2,8 @@ import {finaliseHeader} from "../../header.js";
 import {dimmer, useWhiteText, toRGB} from '../../ui.js';
 import {selector, gradientPuBu, proteinViewer} from "../../signatures.js";
 
-
-function _getDescriptions(proteinsModal) {
-    const url = '/api' + location.pathname + location.search;
-    utils.dimmer(true);
-    utils.getJSON(url, (obj, status) => {
-        // Find the highest protein count
-        const maxProt = Math.max(...obj.data.map(descr => {
-            return Math.max(...descr.methods.map(method => { return method.count }));
-        }));
-
-        // Table header
-        let html = '<thead><tr data-search="db='+ obj.database +'"><th>'+ obj.data.length +' descriptions</th>';
-        if (obj.data.length) {
-            obj.data[0].methods.forEach(method => {
-                html += '<th><a href="" data-method="'+ method.accession +'">' + method.accession + '</a></th>';
-            });
-        }
-        html += '</tr></thead>';
-
-        // Table body
-        const colors = utils.gradientPuBu;
-        html += '</thead><tbody>';
-        obj.data.forEach(descr => {
-            html += '<tr data-type data-filter="'+ descr.value +'" data-search="description=' + descr.id + '&db='+ obj.database +'">' +
-                '<td>'+ descr.value +'</td>';
-
-            descr.methods.forEach(method => {
-                if (method.count) {
-                    const i = Math.floor(method.count / maxProt * colors.length);
-                    const color = colors[Math.min(i, colors.length - 1)];
-                    const className = utils.useWhiteText(color) ? 'light' : 'dark';
-                    html += '<td class="'+ className +'" style="background-color: '+ color +';"><a href="#" data-method="'+ method.accession +'">' + method.count.toLocaleString() + '</a></td>';
-                } else
-                    html += '<td></td>';
-            });
-
-            html += '</tr>';
-        });
-
-        const table = document.querySelector('table');
-        table.innerHTML = html + '</tbody>';
-
-        proteinsModal.observe(table.querySelectorAll('th a[data-method]'), (method, filter, search) => {
-            const header = '<em>' + method + '</em> proteins';
-            proteinsModal.open(method, search, header, true);
-        });
-
-        proteinsModal.observe(table.querySelectorAll('td a[data-method]'), (method, filter, search) => {
-            const header = '<em>' + method + '</em> proteins<div class="sub header">Description: <em>'+ filter +'</em></div>';
-            proteinsModal.open(method, search, header);
-        });
-
-        // Update radios
-        document.querySelector('input[type=radio][value="'+ obj.database +'"]').checked = true;
-
-        utils.dimmer(false);
-    });
-}
-
-
 function getDescriptions(accessions) {
     dimmer(true);
-    const url = new URL(location.href);
-
     fetch("/api" + location.pathname + location.search)
         .then(response => response.json())
         .then(result => {
@@ -123,7 +61,6 @@ function getDescriptions(accessions) {
             dimmer(false);
         });
 }
-
 
 $(function () {
     const match = location.pathname.match(/^\/signatures\/(.+)\/descriptions\/$/i);
