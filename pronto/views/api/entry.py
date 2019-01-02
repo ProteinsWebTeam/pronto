8bcf8d0a655cb27b4c5dc320f31f3cb00b025c46
Matthias Blum <mat.blum@gmail.com>
2019-01-02 23:20:53 +0000

    API side for entry

--- pronto/views/api/entry.py
+++ pronto/views/api/entry.py
@@ -3,7 +3,228 @@ import re
 from cx_Oracle import DatabaseError, IntegrityError
 from flask import jsonify, request
 
-from pronto import app, db, get_user
+from pronto import app, db, get_user, xref
+
+
+@app.route("/api/entry/<accession>/")
+def get_entry(accession):
+    cur = db.get_oracle().cursor()
+    cur.execute(
+        """
+        SELECT
+          E.NAME,
+          E.SHORT_NAME,
+          E.ENTRY_TYPE,
+          ET.ABBREV,
+          E.CHECKED,
+          E.TIMESTAMP
+        FROM INTERPRO.ENTRY E
+        INNER JOIN INTERPRO.CV_ENTRY_TYPE ET 
+          ON E.ENTRY_TYPE = ET.CODE
+        WHERE E.ENTRY_AC = :1
+        """,
+        (accession,)
+    )
+
+    row = cur.fetchone()
+    cur.close()
+    if row:
+        entry = {
+            "accession": accession,
+            "name": row[0],
+            "short_name": row[1],
+            "type": {
+                "code": row[2],
+                "name": row[3].replace('_', ' ')
+            },
+            "is_checked": row[4] == 'Y',
+            "last_modification": row[5].strftime("%d %b %Y")
+        }
+        return jsonify(entry), 200
+    else:
+        return jsonify(None), 404
+
+
+@app.route("/api/entry/<accession>/signatures/")
+def get_entry_signatures(accession):
+    cur = db.get_oracle().cursor()
+    cur.execute(
+        """
+        SELECT
+          DBCODE,
+          METHOD_AC,
+          NAME
+        FROM {0}.METHOD
+        WHERE METHOD_AC IN (
+          SELECT METHOD_AC
+          FROM {0}.ENTRY2METHOD
+          WHERE ENTRY_AC = :1
+        )
+        ORDER BY METHOD_AC
+        """.format(app.config['DB_SCHEMA']),
+        (accession,)
+    )
+
+    signatures = []
+    for row in cur:
+        database = xref.find_ref(row[0], row[1])
+
+        signatures.append({
+            "accession": row[1],
+            "name": row[2],
+            "num_proteins": 0,  # TODO: get count from METHOD.PROTEIN_COUNT
+            "link": database.gen_link(),
+            "color": database.color,
+            "database": database.name
+        })
+
+    cur.close()
+    return jsonify(signatures), 200
+
+
+@app.route("/api/entry/<accession>/go/")
+def get_entry_go_terms(accession):
+    cur = db.get_oracle().cursor()
+
+    cur.execute(
+        """
+        SELECT 
+          T.GO_ID, T.NAME, T.CATEGORY, T.DEFINITION, 
+          T.IS_OBSOLETE, T.REPLACED_BY
+        FROM INTERPRO.INTERPRO2GO I
+          INNER JOIN {}.TERM T ON I.GO_ID = T.GO_ID
+        WHERE I.ENTRY_AC = :1
+        ORDER BY T.GO_ID
+        """.format(app.config["DB_SCHEMA"]),
+        (accession,)
+    )
+
+    terms = []
+    for row in cur:
+        terms.append({
+            "id": row[0],
+            "name": row[1],
+            "category": row[2],
+            "definition": row[3],
+            "is_obsolote": row[4] == 'Y',
+            "replaced_by": row[5]
+        })
+
+    cur.close()
+
+    return jsonify(terms), 200
+
+
+@app.route("/api/entry/<accession>/relationships/")
+def get_entry_relationships(accession):
+    cur = db.get_oracle().cursor()
+
+    cur.execute(
+        """
+        SELECT 
+          R.PARENT_AC, E1.NAME, E1.ENTRY_TYPE, 
+          R.ENTRY_AC, E2.NAME, E2.ENTRY_TYPE
+        FROM (
+            SELECT PARENT_AC, ENTRY_AC
+            FROM {0}.ENTRY2ENTRY
+            START WITH ENTRY_AC = :accession
+            CONNECT BY PRIOR PARENT_AC = ENTRY_AC
+            UNION ALL
+            SELECT PARENT_AC, ENTRY_AC
+            FROM {0}.ENTRY2ENTRY
+            START WITH PARENT_AC = :accession
+            CONNECT BY PRIOR ENTRY_AC = PARENT_AC        
+        ) R
+        INNER JOIN {0}.ENTRY E1 ON R.PARENT_AC = E1.ENTRY_AC
+        INNER JOIN {0}.ENTRY E2 ON R.ENTRY_AC = E2.ENTRY_AC
+        """.format(app.config["DB_SCHEMA"]),
+        dict(accession=accession)
+    )
+
+    parents = {}
+    hierarchy = {}
+    for row in cur:
+        parent_acc = row[0]
+        parent_name = row[1]
+        parent_type = row[2]
+        child_acc = row[3]
+        child_name = row[4]
+        child_type = row[5]
+
+        parents[child_acc] = parent_acc
+
+        if parent_acc in parents:
+            _parents = []
+            root_acc = parent_acc
+            while root_acc in parents:
+                root_acc = parents[parent_acc]
+                _parents.append(root_acc)
+
+            children = {}
+            for acc in reversed(_parents):
+                if not children:
+                    children = hierarchy[acc]["children"]
+                else:
+                    children = children[acc]["children"]
+
+            node = children[parent_acc]
+        elif parent_acc in hierarchy:
+            node = hierarchy[parent_acc]
+        else:
+            node = hierarchy[parent_acc] = {
+                "accession": parent_acc,
+                "name": parent_name,
+                "type": parent_type,
+                "children": {}
+            }
+
+        if child_acc in hierarchy:
+            child = hierarchy.pop(child_acc)
+        else:
+            child = {
+                "accession": child_acc,
+                "name": child_name,
+                "type": child_type,
+                "children": {}
+            }
+
+        node["children"][child_acc] = child
+
+    cur.close()
+    return jsonify(hierarchy), 200
+
+
+@app.route("/api/entry/<accession>/references/")
+def get_entry_references(accession):
+    cur = db.get_oracle().cursor()
+
+    cur.execute(
+        """
+        SELECT 
+          T.GO_ID, T.NAME, T.CATEGORY, T.DEFINITION, 
+          T.IS_OBSOLETE, T.REPLACED_BY
+        FROM INTERPRO.INTERPRO2GO I
+          INNER JOIN {}.TERM T ON I.GO_ID = T.GO_ID
+        WHERE I.ENTRY_AC = :1
+        ORDER BY T.GO_ID
+        """.format(app.config["DB_SCHEMA"]),
+        (accession,)
+    )
+
+    terms = []
+    for row in cur:
+        terms.append({
+            "id": row[0],
+            "name": row[1],
+            "category": row[2],
+            "definition": row[3],
+            "is_obsolote": row[4] == 'Y',
+            "replaced_by": row[5]
+        })
+
+    cur.close()
+
+    return jsonify(terms), 200
 
 
 @app.route("/api/entry/<accession>/check/", methods=["POST"])
