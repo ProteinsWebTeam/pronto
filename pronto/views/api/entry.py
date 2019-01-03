b76a8441322b15cb6c626578debebafc01b14520
Matthias Blum <mat.blum@gmail.com>
2019-01-03 14:40:38 +0000

    Reorganise functions

--- pronto/views/api/entry.py
+++ pronto/views/api/entry.py
@@ -45,87 +45,23 @@ def get_entry(accession):
         return jsonify(None), 404
 
 
-@app.route("/api/entry/<accession>/signatures/")
-def get_entry_signatures(accession):
-    cur = db.get_oracle().cursor()
-    cur.execute(
-        """
-        SELECT
-          DBCODE,
-          METHOD_AC,
-          NAME,
-          PROTEIN_COUNT
-        FROM {0}.METHOD
-        WHERE METHOD_AC IN (
-          SELECT METHOD_AC
-          FROM {0}.ENTRY2METHOD
-          WHERE ENTRY_AC = :1
-        )
-        ORDER BY METHOD_AC
-        """.format(app.config['DB_SCHEMA']),
-        (accession,)
-    )
+@app.route("/api/entry/<accession>/check/", methods=["POST"])
+def check_entry(accession):
+    content = request.get_json()
+    try:
+        is_checked = bool(int(content["checked"]))
+    except (KeyError, ValueError):
+        return jsonify({
+            "status": False,
+            "message": "Invalid or missing parameters."
+        }), 400
 
-    signatures = []
-    for row in cur:
-        database = xref.find_ref(row[0], row[1])
-
-        signatures.append({
-            "accession": row[1],
-            "name": row[2],
-            "num_proteins": row[3],
-            "link": database.gen_link(),
-            "color": database.color,
-            "database": database.name
-        })
-
-    cur.close()
-    return jsonify(signatures), 200
-
-
-@app.route("/api/entry/<accession>/go/")
-def get_entry_go_terms(accession):
-    cur = db.get_oracle().cursor()
-
-    cur.execute(
-        """
-        SELECT 
-          T.GO_ID, T.NAME, T.CATEGORY, T.DEFINITION, 
-          T.IS_OBSOLETE, T.REPLACED_BY
-        FROM INTERPRO.INTERPRO2GO I
-          INNER JOIN {}.TERM T ON I.GO_ID = T.GO_ID
-        WHERE I.ENTRY_AC = :1
-        ORDER BY T.GO_ID
-        """.format(app.config["DB_SCHEMA"]),
-        (accession,)
-    )
-
-    terms = []
-    for row in cur:
-        terms.append({
-            "id": row[0],
-            "name": row[1],
-            "category": row[2],
-            "definition": row[3],
-            "is_obsolete": row[4] == 'Y',
-            "secondary": row[5] is not None
-        })
-
-    cur.close()
-
-    return jsonify(terms), 200
-
-
-@app.route("/api/entry/<accession>/go/<term_id>/", methods=['DELETE'])
-def delete_go_term(accession, term_id):
     user = get_user()
 
     if not user:
         return jsonify({
             "status": False,
-            "title": "Access denied",
-            "message": 'Please <a href="/login/">log in</a> '
-                       'to perform this operation.'
+            "message": "Please log in to perform this action."
         }), 401
 
     con = db.get_oracle()
@@ -133,28 +69,25 @@ def delete_go_term(accession, term_id):
     try:
         cur.execute(
             """
-            DELETE FROM INTERPRO.INTERPRO2GO
-            WHERE ENTRY_AC = :1 AND GO_ID = :2
+            UPDATE INTERPRO.ENTRY
+            SET CHECKED = :1
+            WHERE ENTRY_AC = :2
             """,
-            (accession, term_id)
+            ('Y' if is_checked else 'N', accession)
         )
-    except IntegrityError:
+    except DatabaseError as e:
+        cur.close()
         return jsonify({
             "status": False,
-            "title": "Database error",
-            "message": "Could not delete GO term {} "
-                       "from InterPro entry {}".format(accession, term_id)
+            "message": "Could not update {}: {}".format(accession, e)
         }), 400
     else:
-        # row_count = cur.rowcount  # TODO: check that row_count == 1?
         con.commit()
+        cur.close()
         return jsonify({
             "status": True,
-            "title": None,
             "message": None
         }), 200
-    finally:
-        cur.close()
 
 
 @app.route("/api/entry/<parent_acc>/child/<child_acc>/", methods=["PUT"])
@@ -240,8 +173,179 @@ def add_child_relationship(parent_acc, child_acc):
         cur.close()
 
 
-@app.route("/api/entry/<acc1>/relationship/<acc2>/", methods=['DELETE'])
-def delete_relationship(acc1, acc2):
+@app.route("/api/entry/<accession>/comment/", methods=["PUT"])
+def add_entry_comment(accession):
+    user = get_user()
+    if not user:
+        return jsonify({
+            "status": False,
+            "message": "Please log in to perform this action."
+        }), 401
+
+    content = request.get_json()
+    text = content.get("text", "")
+    if len(text) < 3:
+        return jsonify({
+            "status": False,
+            "message": "Comment too short (must be at least "
+                       "three characters long)."
+        }), 400
+
+    con = db.get_oracle()
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT MAX(ID)
+        FROM INTERPRO.ENTRY_COMMENT
+        """
+    )
+    max_id = cur.fetchone()[0]
+    next_id = max_id + 1 if max_id else 1
+
+    try:
+        cur.execute(
+            """
+            INSERT INTO INTERPRO.ENTRY_COMMENT (
+              ID, ENTRY_AC, USERNAME, VALUE
+            )
+            VALUES (:1, :2, :3, :4)
+            """,
+            (next_id, accession, user["username"], text)
+        )
+    except IntegrityError as e:
+        cur.close()
+        return jsonify({
+            "status": False,
+            "message": "Could not add comment "
+                       "for {}: {}".format(accession, e)
+        }), 400
+    else:
+        con.commit()
+        cur.close()
+        return jsonify({
+            "status": True,
+            "message": None
+        }), 200
+
+
+@app.route("/api/entry/<accession>/comment/<_id>/", methods=["DELETE"])
+def delete_entry_comment(accession, _id):
+    user = get_user()
+    if not user:
+        return jsonify({
+            "status": False,
+            "message": "Please log in to perform this action."
+        }), 401
+
+    con = db.get_oracle()
+    cur = con.cursor()
+    try:
+        cur.execute(
+            """
+            UPDATE INTERPRO.ENTRY_COMMENT 
+            SET STATUS = 'N' 
+            WHERE ENTRY_AC = :1 AND ID = :2
+            """,
+            (accession, _id)
+        )
+    except DatabaseError as e:
+        cur.close()
+        return jsonify({
+            "status": False,
+            "message": "Could not delete comment "
+                       "for {}: {}".format(accession, e)
+        }), 400
+    else:
+        con.commit()
+        cur.close()
+        return jsonify({
+            "status": True,
+            "message": None
+        }), 200
+
+
+@app.route("/api/entry/<accession>/comments/")
+def get_entry_comments(accession):
+    try:
+        n = int(request.args["max"])
+    except (KeyError, ValueError):
+        n = 0
+
+    cur = db.get_oracle().cursor()
+    cur.execute(
+        """
+        SELECT C.ID, C.VALUE, C.CREATED_ON, C.STATUS, U.NAME
+        FROM INTERPRO.ENTRY_COMMENT C
+        INNER JOIN INTERPRO.USER_PRONTO U ON C.USERNAME = U.USERNAME
+        WHERE C.ENTRY_AC = :1
+        ORDER BY C.CREATED_ON DESC
+        """,
+        (accession,)
+    )
+
+    comments = [
+        {
+            "id": row[0],
+            "text": row[1],
+            "date": row[2].strftime("%Y-%m-%d %H:%M:%S"),
+            "status": row[3] == "Y",
+            "author": row[4],
+        } for row in cur
+    ]
+    cur.close()
+    n_comments = len(comments)
+
+    if n:
+        comments = comments[:n]
+
+    for c in comments:
+        c["text"] = re.sub(
+            r"#(\d+)",
+            r'<a href="https://github.com/geneontology/go-annotation/issues/\1">#\1</a>',
+            c["text"]
+        )
+
+    return jsonify({
+        "count": n_comments,
+        "comments": comments
+    })
+
+
+@app.route("/api/entry/<accession>/go/")
+def get_entry_go_terms(accession):
+    cur = db.get_oracle().cursor()
+
+    cur.execute(
+        """
+        SELECT 
+          T.GO_ID, T.NAME, T.CATEGORY, T.DEFINITION, 
+          T.IS_OBSOLETE, T.REPLACED_BY
+        FROM INTERPRO.INTERPRO2GO I
+          INNER JOIN {}.TERM T ON I.GO_ID = T.GO_ID
+        WHERE I.ENTRY_AC = :1
+        ORDER BY T.GO_ID
+        """.format(app.config["DB_SCHEMA"]),
+        (accession,)
+    )
+
+    terms = []
+    for row in cur:
+        terms.append({
+            "id": row[0],
+            "name": row[1],
+            "category": row[2],
+            "definition": row[3],
+            "is_obsolete": row[4] == 'Y',
+            "secondary": row[5] is not None
+        })
+
+    cur.close()
+
+    return jsonify(terms), 200
+
+
+@app.route("/api/entry/<accession>/go/<term_id>/", methods=['DELETE'])
+def delete_go_term(accession, term_id):
     user = get_user()
 
     if not user:
@@ -257,17 +361,17 @@ def delete_relationship(acc1, acc2):
     try:
         cur.execute(
             """
-            DELETE FROM INTERPRO.ENTRY2ENTRY
-            WHERE (PARENT_AC = :acc1 AND ENTRY_AC = :acc2)
-            OR (PARENT_AC = :acc2 AND ENTRY_AC = :acc1)
+            DELETE FROM INTERPRO.INTERPRO2GO
+            WHERE ENTRY_AC = :1 AND GO_ID = :2
             """,
-            dict(acc1=acc1, acc2=acc2)
+            (accession, term_id)
         )
     except IntegrityError:
         return jsonify({
             "status": False,
             "title": "Database error",
-            "message": "Could not delete unlink {} and {}".format(acc1, acc2)
+            "message": "Could not delete GO term {} "
+                       "from InterPro entry {}".format(accession, term_id)
         }), 400
     else:
         # row_count = cur.rowcount  # TODO: check that row_count == 1?
@@ -359,6 +463,53 @@ def add_go_term(accession, term_id):
         cur.close()
 
 
+@app.route("/api/entry/<accession>/references/")
+def get_entry_references(accession):
+    pass
+    # todo implement
+
+
+@app.route("/api/entry/<acc1>/relationship/<acc2>/", methods=['DELETE'])
+def delete_relationship(acc1, acc2):
+    user = get_user()
+
+    if not user:
+        return jsonify({
+            "status": False,
+            "title": "Access denied",
+            "message": 'Please <a href="/login/">log in</a> '
+                       'to perform this operation.'
+        }), 401
+
+    con = db.get_oracle()
+    cur = con.cursor()
+    try:
+        cur.execute(
+            """
+            DELETE FROM INTERPRO.ENTRY2ENTRY
+            WHERE (PARENT_AC = :acc1 AND ENTRY_AC = :acc2)
+            OR (PARENT_AC = :acc2 AND ENTRY_AC = :acc1)
+            """,
+            dict(acc1=acc1, acc2=acc2)
+        )
+    except IntegrityError:
+        return jsonify({
+            "status": False,
+            "title": "Database error",
+            "message": "Could not delete unlink {} and {}".format(acc1, acc2)
+        }), 400
+    else:
+        # row_count = cur.rowcount  # TODO: check that row_count == 1?
+        con.commit()
+        return jsonify({
+            "status": True,
+            "title": None,
+            "message": None
+        }), 200
+    finally:
+        cur.close()
+
+
 @app.route("/api/entry/<accession>/relationships/")
 def get_entry_relationships(accession):
     cur = db.get_oracle().cursor()
@@ -440,217 +591,40 @@ def get_entry_relationships(accession):
     return jsonify(hierarchy), 200
 
 
-@app.route("/api/entry/<accession>/references/")
-def get_entry_references(accession):
+@app.route("/api/entry/<accession>/signatures/")
+def get_entry_signatures(accession):
     cur = db.get_oracle().cursor()
-
     cur.execute(
         """
-        SELECT 
-          T.GO_ID, T.NAME, T.CATEGORY, T.DEFINITION, 
-          T.IS_OBSOLETE, T.REPLACED_BY
-        FROM INTERPRO.INTERPRO2GO I
-          INNER JOIN {}.TERM T ON I.GO_ID = T.GO_ID
-        WHERE I.ENTRY_AC = :1
-        ORDER BY T.GO_ID
-        """.format(app.config["DB_SCHEMA"]),
+        SELECT
+          DBCODE,
+          METHOD_AC,
+          NAME,
+          PROTEIN_COUNT
+        FROM {0}.METHOD
+        WHERE METHOD_AC IN (
+          SELECT METHOD_AC
+          FROM {0}.ENTRY2METHOD
+          WHERE ENTRY_AC = :1
+        )
+        ORDER BY METHOD_AC
+        """.format(app.config['DB_SCHEMA']),
         (accession,)
     )
 
-    terms = []
+    signatures = []
     for row in cur:
-        terms.append({
-            "id": row[0],
-            "name": row[1],
-            "category": row[2],
-            "definition": row[3],
-            "is_obsolote": row[4] == 'Y',
-            "replaced_by": row[5]
+        database = xref.find_ref(row[0], row[1])
+
+        signatures.append({
+            "accession": row[1],
+            "name": row[2],
+            "num_proteins": row[3],
+            "link": database.gen_link(),
+            "color": database.color,
+            "database": database.name
         })
 
     cur.close()
+    return jsonify(signatures), 200
 
-    return jsonify(terms), 200
-
-
-@app.route("/api/entry/<accession>/check/", methods=["POST"])
-def check_entry(accession):
-    content = request.get_json()
-    try:
-        is_checked = bool(int(content["checked"]))
-    except (KeyError, ValueError):
-        return jsonify({
-            "status": False,
-            "message": "Invalid or missing parameters."
-        }), 400
-
-    user = get_user()
-
-    if not user:
-        return jsonify({
-            "status": False,
-            "message": "Please log in to perform this action."
-        }), 401
-
-    con = db.get_oracle()
-    cur = con.cursor()
-    try:
-        cur.execute(
-            """
-            UPDATE INTERPRO.ENTRY
-            SET CHECKED = :1
-            WHERE ENTRY_AC = :2
-            """,
-            ('Y' if is_checked else 'N', accession)
-        )
-    except DatabaseError as e:
-        cur.close()
-        return jsonify({
-            "status": False,
-            "message": "Could not update {}: {}".format(accession, e)
-        }), 400
-    else:
-        con.commit()
-        cur.close()
-        return jsonify({
-            "status": True,
-            "message": None
-        }), 200
-
-
-@app.route("/api/entry/<accession>/comments/")
-def get_entry_comments(accession):
-    try:
-        n = int(request.args["max"])
-    except (KeyError, ValueError):
-        n = 0
-
-    cur = db.get_oracle().cursor()
-    cur.execute(
-        """
-        SELECT C.ID, C.VALUE, C.CREATED_ON, C.STATUS, U.NAME
-        FROM INTERPRO.ENTRY_COMMENT C
-        INNER JOIN INTERPRO.USER_PRONTO U ON C.USERNAME = U.USERNAME
-        WHERE C.ENTRY_AC = :1
-        ORDER BY C.CREATED_ON DESC
-        """,
-        (accession,)
-    )
-
-    comments = [
-        {
-            "id": row[0],
-            "text": row[1],
-            "date": row[2].strftime("%Y-%m-%d %H:%M:%S"),
-            "status": row[3] == "Y",
-            "author": row[4],
-        } for row in cur
-    ]
-    cur.close()
-    n_comments = len(comments)
-
-    if n:
-        comments = comments[:n]
-
-    for c in comments:
-        c["text"] = re.sub(
-            r"#(\d+)",
-            r'<a href="https://github.com/geneontology/go-annotation/issues/\1">#\1</a>',
-            c["text"]
-        )
-
-    return jsonify({
-        "count": n_comments,
-        "comments": comments
-    })
-
-
-@app.route("/api/entry/<accession>/comment/<_id>/", methods=["DELETE"])
-def delete_entry_comment(accession, _id):
-    user = get_user()
-    if not user:
-        return jsonify({
-            "status": False,
-            "message": "Please log in to perform this action."
-        }), 401
-
-    con = db.get_oracle()
-    cur = con.cursor()
-    try:
-        cur.execute(
-            """
-            UPDATE INTERPRO.ENTRY_COMMENT 
-            SET STATUS = 'N' 
-            WHERE ENTRY_AC = :1 AND ID = :2
-            """,
-            (accession, _id)
-        )
-    except DatabaseError as e:
-        cur.close()
-        return jsonify({
-            "status": False,
-            "message": "Could not delete comment "
-                       "for {}: {}".format(accession, e)
-        }), 400
-    else:
-        con.commit()
-        cur.close()
-        return jsonify({
-            "status": True,
-            "message": None
-        }), 200
-
-
-@app.route("/api/entry/<accession>/comment/", methods=["PUT"])
-def add_entry_comment(accession):
-    user = get_user()
-    if not user:
-        return jsonify({
-            "status": False,
-            "message": "Please log in to perform this action."
-        }), 401
-
-    content = request.get_json()
-    text = content.get("text", "")
-    if len(text) < 3:
-        return jsonify({
-            "status": False,
-            "message": "Comment too short (must be at least "
-                       "three characters long)."
-        }), 400
-
-    con = db.get_oracle()
-    cur = con.cursor()
-    cur.execute(
-        """
-        SELECT MAX(ID)
-        FROM INTERPRO.ENTRY_COMMENT
-        """
-    )
-    max_id = cur.fetchone()[0]
-    next_id = max_id + 1 if max_id else 1
-
-    try:
-        cur.execute(
-            """
-            INSERT INTO INTERPRO.ENTRY_COMMENT (
-              ID, ENTRY_AC, USERNAME, VALUE
-            )
-            VALUES (:1, :2, :3, :4)
-            """,
-            (next_id, accession, user["username"], text)
-        )
-    except IntegrityError as e:
-        cur.close()
-        return jsonify({
-            "status": False,
-            "message": "Could not add comment "
-                       "for {}: {}".format(accession, e)
-        }), 400
-    else:
-        con.commit()
-        cur.close()
-        return jsonify({
-            "status": True,
-            "message": None
-        }), 200
