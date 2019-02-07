95fa2d56a8bef426c633750ed66cad0ea12ce41b
Matthias Blum <mat.blum@gmail.com>
2019-02-07 17:20:32 +0000

    Perf improvement

--- pronto/views/api/signatures/taxonomy.py
+++ pronto/views/api/signatures/taxonomy.py
@@ -1,10 +1,97 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from flask import jsonify, request
 
 from pronto import app, db
 
 
+def _get_proteins(cur, accession, rank, taxon_id):
+    cur.execute(
+        """
+        SELECT MP.PROTEIN_AC
+          FROM (
+            SELECT PROTEIN_AC, LEFT_NUMBER
+            FROM {0}.METHOD2PROTEIN
+            WHERE METHOD_AC = :1
+          ) MP
+        INNER JOIN {0}.LINEAGE L 
+          ON MP.LEFT_NUMBER = L.LEFT_NUMBER 
+          AND L.RANK = :2
+          AND L.TAX_ID = :3
+        """.format(app.config["DB_SCHEMA"]),
+        (accession, rank, taxon_id)
+    )
+
+    return {row[0] for row in cur}
+
+
 @app.route("/api/signatures/<path:accessions_str>/taxonomy/<rank>/<int:taxon_id>/")
 def get_taxa_common_proteins(accessions_str, rank, taxon_id):
+    common = None
+    cur = db.get_oracle().cursor()
+    for acc in accessions_str.split("/"):
+        if common is None:
+            common = _get_proteins(cur, acc, rank, taxon_id)
+        else:
+            common &= _get_proteins(cur, acc, rank, taxon_id)
+
+    proteins = []
+    if common:
+        cur.execute(
+            """
+            SELECT 
+              P.PROTEIN_AC, P.DBCODE, P.LEN, P.NAME, 
+              DV.TEXT, E.TAX_ID, E.FULL_NAME
+            FROM {0}.PROTEIN P
+            INNER JOIN {0}.PROTEIN_DESC PD 
+              ON P.PROTEIN_AC = PD.PROTEIN_AC
+            INNER JOIN {0}.DESC_VALUE DV
+              ON PD.DESC_ID = DV.DESC_ID
+            INNER JOIN {0}.ETAXI E
+              ON P.TAX_ID = E.TAX_ID
+            WHERE P.PROTEIN_AC IN ({1})
+            ORDER BY P.PROTEIN_AC
+            """.format(
+                app.config["DB_SCHEMA"],
+                ','.join([":" + str(i+1) for i in range(len(common))])
+            ),
+            tuple(common)
+        )
+
+        for row in cur:
+            protein_acc = row[0]
+            if row[1] == 'S':
+                is_reviewed = True
+                link = "//sp.isb-sib.ch/uniprot/{}".format(protein_acc)
+            else:
+                is_reviewed = False
+                link = "//www.uniprot.org/uniprot/{}".format(protein_acc)
+
+            proteins.append({
+                "accession": protein_acc,
+                "reviewed": is_reviewed,
+                "link": link,
+                "length": row[2],
+                "identifier": row[3],
+                "name": row[4],
+                "taxon": {
+                    "id": row[5],
+                    "name": row[6]
+                }
+            })
+
+    cur.close()
+    return jsonify({
+        "count": len(proteins),
+        "proteins": proteins,
+        "page_info": {
+            "page": 1,
+            "page_size": len(proteins)
+        }
+    })
+
+    return
+
     params = {
         "rank": rank,
         "taxid": taxon_id
