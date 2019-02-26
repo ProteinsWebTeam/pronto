7e23388b9aa599235815bf868d9dbb09d7604891
Matthias Blum <mat.blum@gmail.com>
2019-02-26 14:09:36 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='pronto',
-    version='1.3',
-    packages=['pronto'],
+    name="pronto",
+    version="1.3",
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'cx_Oracle>=5.3',
-        'Flask>=0.12.2',
-        'Jinja2>=2.10',
-        'mysqlclient>=1.3'
+        "cx_Oracle>=6.0.2",
+        "Flask>=0.12.2",
+        "mysqlclient>=1.3.10"
     ],
 )
