968b32defd8d563160b5b41dc05906e86148c13e
Matthias Blum <mat.blum@gmail.com>
2019-05-23 18:24:54 +0100

    Give ID to table

--- pronto/static/modules/search.js
+++ pronto/static/modules/search.js
@@ -32,7 +32,7 @@ function callEBISearch(query, page, pageSize) {
                 html = '<tr><td colspan="3" class="center aligned">No results found for <strong>'+ query +'</strong></td></tr>';
             }
 
-            const table = document.querySelector("table");
+            const table = document.querySelector("#search-results");
             table.querySelector("tbody").innerHTML = html;
             ui.paginate(table, page, pageSize, result.hitCount, (newHref, ) => {
                 const newURL = new URL(newHref);
