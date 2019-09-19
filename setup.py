222649c7271540b95045b9f414ed61c0ef7dc15a
Matthias Blum <mat.blum@gmail.com>
2019-09-19 12:48:58 +0100

    Get version from __init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
-from .pronto import __version__
+
+
+def get_version():
+    path = os.path.join(os.path.dirname(__file__), "pronto", "__init__.py")
+    with open(path, "rt") as fh:
+        for line in fh:
+            if line.startswith("__version__"):
+                return line.split('=', 1)[1].strip()
 
 
 setup(
     name="pronto",
-    version=__version__,
+    version=get_version(),
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
