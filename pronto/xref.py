34fd2a228e44a76b0af844710722ba28470a7923
Matthias Blum <mat.blum@gmail.com>
2019-09-25 17:39:22 +0100

    Updating colours as several databases are not used any more

--- pronto/xref.py
+++ pronto/xref.py
@@ -15,12 +15,138 @@ class _Database:
         raise NotImplementedError()
 
 
+class _CathGene3D(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'CATH-Gene3D'
+        self.home = 'http://www.cathdb.info'
+        self.color = '#d9417c'
+
+    def gen_link(self):
+        return self.home + '/superfamily/' + re.match(r'G3DSA:(.+)', self.ac).group(1)
+
+
+class _Cdd(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'CDD'
+        self.home = 'http://www.ncbi.nlm.nih.gov/Structure/cdd/cdd.shtml'
+        self.color = '#c6554b'
+
+    def gen_link(self):
+        return 'http://www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid=' + self.ac
+
+
+class _Hamap(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'HAMAP'
+        self.home = 'http://hamap.expasy.org'
+        self.color = '#ce672d'
+
+    def gen_link(self):
+        return self.home + '/profile/' + self.ac
+
+
+class _MobiDbLite(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'MobiDB Lite'
+        self.home = 'http://mobidb.bio.unipd.it'
+        self.color = '#cca14a'
+
+    def gen_link(self):
+        return self.home + '/entries/' + self.ac
+
+
+class _Panther(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'PANTHER'
+        self.home = 'http://www.pantherdb.org'
+        self.color = '#777934'
+
+    def gen_link(self):
+        return self.home + '/panther/family.do?clsAccession=' + self.ac
+
+
+class _Pfam(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'Pfam'
+        self.home = 'http://pfam.xfam.org'
+        self.color = '#77b341'
+
+    def gen_link(self):
+        return self.home + '/family/' + self.ac
+
+
+class _Pirsf(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'PIRSF'
+        self.home = 'http://pir.georgetown.edu/pirwww/dbinfo/pirsf.shtml'
+        self.color = '#51ac7e'
+
+    def gen_link(self):
+        return 'http://pir.georgetown.edu/cgi-bin/ipcSF?id=' + self.ac
+
+
+class _Prints(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'PRINTS'
+        self.home = 'http://www.bioinf.manchester.ac.uk/dbbrowser/PRINTS'
+        self.color = '#59a0d5'
+
+    def gen_link(self):
+        return ('http://www.bioinf.manchester.ac.uk/cgi-bin/dbbrowser/sprint/searchprintss.cgi?prints_accn={}'
+                '&display_opts=Prints&category=None&queryform=false&regexpr=off'.format(self.ac))
+
+
+"""
+# Prodom has been removed in May 2019
+
+class _Prodom(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'ProDom'
+        self.home = 'http://prodom.prabi.fr'
+        self.color = '#76b84a'
+
+    def gen_link(self):
+        return self.home + '/prodom/current/cgi-bin/request.pl?question=DBEN&query=' + self.ac
+"""
+
+
+class _PrositePatterms(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'PROSITE patterns'
+        self.home = 'http://prosite.expasy.org'
+        self.color = '#596dce'
+
+    def gen_link(self):
+        return self.home + '/' + self.ac
+
+
+class _PrositeProfiles(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'PROSITE profiles'
+        self.home = 'http://prosite.expasy.org'
+        self.color = '#9e76bb'
+
+    def gen_link(self):
+        return self.home + '/' + self.ac
+
+
 class _Sfld(_Database):
     def __init__(self, ac):
         super().__init__(ac)
         self.name = 'SFLD'
         self.home = 'http://sfld.rbvi.ucsf.edu/django'
-        self.color = '#6175c3'
+        self.color = '#9958cb'
 
     def gen_link(self):
         if self.ac.startswith('SFLDF'):
@@ -33,137 +159,15 @@ class _Sfld(_Database):
         return self.home + suffix
 
 
-class _Prodom(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'ProDom'
-        self.home = 'http://prodom.prabi.fr'
-        self.color = '#76b84a'
-
-    def gen_link(self):
-        return self.home + '/prodom/current/cgi-bin/request.pl?question=DBEN&query=' + self.ac
-
-
-class _Prints(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'PRINTS'
-        self.home = 'http://www.bioinf.manchester.ac.uk/dbbrowser/PRINTS'
-        self.color = '#6a863e'
-
-    def gen_link(self):
-        return ('http://www.bioinf.manchester.ac.uk/cgi-bin/dbbrowser/sprint/searchprintss.cgi?prints_accn={}'
-                '&display_opts=Prints&category=None&queryform=false&regexpr=off'.format(self.ac))
-
-
-class _Pfam(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'Pfam'
-        self.home = 'http://pfam.xfam.org'
-        self.color = '#a36b33'
-
-    def gen_link(self):
-        return self.home + '/family/' + self.ac
-
-
-class _Cdd(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'CDD'
-        self.home = 'http://www.ncbi.nlm.nih.gov/Structure/cdd/cdd.shtml'
-        self.color = '#a24863'
-
-    def gen_link(self):
-        return 'http://www.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi?uid=' + self.ac
-
-
-class _PrositeProfiles(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'PROSITE profiles'
-        self.home = 'http://prosite.expasy.org'
-        self.color = '#60a2d9'
-
-    def gen_link(self):
-        return self.home + '/' + self.ac
-
-
-class _Tigrfams(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'TIGRFAMs'
-        self.home = 'http://www.jcvi.org/cgi-bin/tigrfams/index.cgi'
-        self.color = '#d18dcd'
-
-    def gen_link(self):
-        return 'http://www.jcvi.org/cgi-bin/tigrfams/HmmReportPage.cgi?acc=' + self.ac
-
-
-class _PrositePatterms(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'PROSITE patterns'
-        self.home = 'http://prosite.expasy.org'
-        self.color = '#4db395'
-
-    def gen_link(self):
-        return self.home + '/' + self.ac
-
-
-class _Hamap(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'HAMAP'
-        self.home = 'http://hamap.expasy.org'
-        self.color = '#e1827a'
-
-    def gen_link(self):
-        return self.home + '/profile/' + self.ac
-
-
 class _Smart(_Database):
     def __init__(self, ac):
         super().__init__(ac)
         self.name = 'SMART'
         self.home = 'http://smart.embl-heidelberg.de'
-        self.color = '#7e63d7'
+        self.color = '#cd56b5'
 
     def gen_link(self):
         return self.home + '/smart/do_annotation.pl?ACC={}&BLAST=DUMMY'.format(self.ac)
-
-
-class _Pirsf(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'PIRSF'
-        self.home = 'http://pir.georgetown.edu/pirwww/dbinfo/pirsf.shtml'
-        self.color = '#d0a242'
-
-    def gen_link(self):
-        return 'http://pir.georgetown.edu/cgi-bin/ipcSF?id=' + self.ac
-
-
-class _Panther(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'PANTHER'
-        self.home = 'http://www.pantherdb.org'
-        self.color = '#d04d33'
-
-    def gen_link(self):
-        return self.home + '/panther/family.do?clsAccession=' + self.ac
-
-
-class _CathGene3D(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'CATH-Gene3D'
-        self.home = 'http://www.cathdb.info'
-        self.color = '#da467b'
-
-    def gen_link(self):
-        return self.home + '/superfamily/' + re.match(r'G3DSA:(.+)', self.ac).group(1)
 
 
 class _Superfamily(_Database):
@@ -171,44 +175,25 @@ class _Superfamily(_Database):
         super().__init__(ac)
         self.name = 'SUPERFAMILY'
         self.home = 'http://supfam.org/SUPERFAMILY'
-        self.color = '#90519d'
+        self.color = '#e288a9'
 
     def gen_link(self):
         return self.home + '/cgi-bin/scop.cgi?ipid=' + self.ac
 
 
-class _MobiDbLite(_Database):
+class _Tigrfams(_Database):
     def __init__(self, ac):
         super().__init__(ac)
-        self.name = 'MobiDB Lite'
-        self.home = 'http://mobidb.bio.unipd.it'
-        self.color = '#c952bc'
+        self.name = 'TIGRFAMs'
+        self.home = 'http://www.jcvi.org/cgi-bin/tigrfams/index.cgi'
+        self.color = '#a04867'
 
     def gen_link(self):
-        return self.home + '/entries/' + self.ac
+        return 'http://www.jcvi.org/cgi-bin/tigrfams/HmmReportPage.cgi?acc=' + self.ac
 
 
-class _ModBase(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'ModBase'
-        self.home = 'http://modbase.compbio.ucsf.edu/modbase-cgi/index.cgi'
-        self.color = '#1b9e77'
-
-    def gen_link(self):
-        return 'http://modbase.compbio.ucsf.edu/modbase-cgi-new/model_search.cgi?searchvalue={}&searchproperties=database_id&displaymode=moddetail&searchmode=default'.format(self.ac[3:])
-
-
-class _PDB(_Database):
-    def __init__(self, ac):
-        super().__init__(ac)
-        self.name = 'PDB'
-        self.home = 'http://www.ebi.ac.uk/pdbe'
-        self.color = '#d95f02'
-
-    def gen_link(self):
-        return 'http://www.ebi.ac.uk/pdbe-srv/view/entry/{}/summary'.format(self.ac)
-
+"""
+# Defuncts databases (or not displayed in Pronto any more)
 
 class _CATH(_Database):
     def __init__(self, ac):
@@ -221,6 +206,30 @@ class _CATH(_Database):
         return self.home + '/superfamily/' + self.ac
 
 
+class _ModBase(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'ModBase'
+        self.home = 'http://modbase.compbio.ucsf.edu/modbase-cgi/index.cgi'
+        self.color = '#1b9e77'
+
+    def gen_link(self):
+        return 'http://modbase.compbio.ucsf.edu/modbase-cgi-new/model_search.cgi?searchvalue={}&searchproperties=database_id&displaymode=moddetail&searchmode=default'.format(
+            self.ac[3:])
+
+
+class _PDB(_Database):
+    def __init__(self, ac):
+        super().__init__(ac)
+        self.name = 'PDB'
+        self.home = 'http://www.ebi.ac.uk/pdbe'
+        self.color = '#d95f02'
+
+    def gen_link(self):
+        return 'http://www.ebi.ac.uk/pdbe-srv/view/entry/{}/summary'.format(
+            self.ac)
+
+
 class _SwissModel(_Database):
     def __init__(self, ac):
         super().__init__(ac)
@@ -229,7 +238,8 @@ class _SwissModel(_Database):
         self.color = '#e7298a'
 
     def gen_link(self):
-        return self.home + '/repository/?pid=smr03&query_1_input=' + self.ac[3::]
+        return self.home + '/repository/?pid=smr03&query_1_input=' + self.ac[
+                                                                     3::]
 
 
 class _SCOP(_Database):
@@ -241,13 +251,14 @@ class _SCOP(_Database):
 
     def gen_link(self):
         return self.home + '//search.cgi?key=/' + self.ac
+"""
 
 
 def find_ref(dbcode, ac=None):
     codes = {
         # Method databases
         'B': _Sfld,
-        'D': _Prodom,
+        # 'D': _Prodom,
         'F': _Prints,
         'H': _Pfam,
         'J': _Cdd,
@@ -262,12 +273,12 @@ def find_ref(dbcode, ac=None):
         'Y': _Superfamily,
         'g': _MobiDbLite,
 
-        # Structural databases
-        'A': _ModBase,
-        'b': _PDB,
-        'h': _CATH,
-        'W': _SwissModel,
-        'y': _SCOP
+        # # Structural databases
+        # 'A': _ModBase,
+        # 'b': _PDB,
+        # 'h': _CATH,
+        # 'W': _SwissModel,
+        # 'y': _SCOP
     }
 
     _DbClass = codes.get(dbcode)
