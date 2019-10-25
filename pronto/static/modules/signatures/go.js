5c3719986abef12c616a232895e04095cc837dc4
Matthias Blum <mat.blum@gmail.com>
2019-10-25 15:47:16 +0100

    Closes #20 (matches for reviewed proteins displayed first)

--- pronto/static/modules/signatures/go.js
+++ pronto/static/modules/signatures/go.js
@@ -29,7 +29,7 @@ function getGoTerms(accessions) {
             // Table body
             html += '<tbody>';
             result.terms.forEach(t => {
-                html += '<tr data-type="GO term" data-filter="'+ t.name +'" data-params="go='+ t.id +'">'
+                html += '<tr data-type="GO term" data-filter="'+ t.name +'" data-params="go='+ t.id +'&reviewedfirst=yes">'
                     + '<td>'
                     + '<span class="ui circular small label aspect-'+ t.aspect +'">'+ t.aspect +'</span>'
                     + '<a target="_blank" href="//www.ebi.ac.uk/QuickGO/term/'+ t.id +'">'
