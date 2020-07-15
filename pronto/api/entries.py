3ee894e341f85603be270cfe9b88d4f11cd0e63a
Matthias Blum <mat.blum@gmail.com>
2020-07-15 17:34:55 +0100

    Showing list of unchecked entries

--- pronto/api/entries.py
+++ pronto/api/entries.py
@@ -8,18 +8,23 @@ from pronto import utils
 bp = Blueprint("api.entries", __name__, url_prefix="/api/entries")
 
 
-@bp.route("/")
-def get_recent_entries():
-    con = utils.connect_oracle()
-    cur = con.cursor()
+def _get_unfreeze_date(cur, days=7):
     cur.execute(
-        """
-        SELECT FILE_DATE - INTERVAL '7' DAY
+        f"""
+        SELECT FILE_DATE - INTERVAL '{days}' DAY
         FROM INTERPRO.DB_VERSION
         WHERE DBCODE = 'I'
         """
     )
     date, = cur.fetchone()
+    return date
+
+
+@bp.route("/news/")
+def get_recent_entries():
+    con = utils.connect_oracle()
+    cur = con.cursor()
+    date = _get_unfreeze_date(cur)
     cur.execute(
         """
         SELECT
@@ -27,11 +32,12 @@ def get_recent_entries():
           NVL(U.NAME, A.DBUSER), E.CHECKED, NVL(EM.NUM_METHODS, 0)
         FROM INTERPRO.ENTRY E
         INNER JOIN (
+          -- First audit event
           SELECT
             ENTRY_AC, DBUSER, TIMESTAMP,
             ROW_NUMBER() OVER (
               PARTITION BY ENTRY_AC
-              ORDER BY TIMESTAMP ASC
+              ORDER BY TIMESTAMP
             ) RN
           FROM INTERPRO.ENTRY_AUDIT
         ) A ON E.ENTRY_AC = A.ENTRY_AC AND A.RN = 1
@@ -52,7 +58,7 @@ def get_recent_entries():
             "type": row[1],
             "short_name": row[2],
             "date": row[3].strftime("%d %b %Y"),
-            "author": row[4],
+            "user": row[4],
             "checked": row[5] == 'Y',
             "signatures": row[6]
         })
@@ -62,3 +68,64 @@ def get_recent_entries():
         "date": date.strftime("%d %b %Y"),
         "entries": entries,
     })
+
+
+@bp.route("/unchecked/")
+def get_unchecked_entries():
+    con = utils.connect_oracle()
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT * FROM (
+            SELECT
+              E.ENTRY_AC, E.ENTRY_TYPE, E.SHORT_NAME, 
+              NVL(FAE.TIMESTAMP, E.TIMESTAMP) AS CREATED_TIME,
+              NVL(LAE.TIMESTAMP, E.TIMESTAMP) AS UPDATED_TIME, 
+              NVL(U.NAME, E.USERSTAMP), NVL(EM.NUM_METHODS, 0) AS NUM_METHODS
+            FROM INTERPRO.ENTRY E
+            LEFT OUTER JOIN (
+              -- First audit event
+              SELECT
+                ENTRY_AC, TIMESTAMP,
+                ROW_NUMBER() OVER (
+                  PARTITION BY ENTRY_AC
+                  ORDER BY TIMESTAMP
+                ) RN
+              FROM INTERPRO.ENTRY_AUDIT
+            ) FAE ON E.ENTRY_AC = FAE.ENTRY_AC AND FAE.RN = 1
+            LEFT OUTER JOIN (
+                -- Latest audit event when entry was unchecked
+                SELECT 
+                    ENTRY_AC, DBUSER, TIMESTAMP, 
+                    ROW_NUMBER() OVER (PARTITION BY ENTRY_AC 
+                                       ORDER BY TIMESTAMP DESC) RN
+                FROM INTERPRO.ENTRY_AUDIT
+                WHERE CHECKED = 'N' 
+            ) LAE ON E.ENTRY_AC = LAE.ENTRY_AC AND LAE.RN = 1
+            LEFT OUTER JOIN INTERPRO.PRONTO_USER U 
+                ON LAE.DBUSER = U.DB_USER
+            LEFT OUTER JOIN (
+              SELECT ENTRY_AC, COUNT(*) AS NUM_METHODS
+              FROM INTERPRO.ENTRY2METHOD
+              GROUP BY ENTRY_AC
+            ) EM ON E.ENTRY_AC = EM.ENTRY_AC
+            WHERE E.CHECKED = 'N'
+        ) E
+        WHERE E.NUM_METHODS > 0
+        ORDER BY UPDATED_TIME
+        """
+    )
+    entries = []
+    for row in cur:
+        entries.append({
+            "accession": row[0],
+            "type": row[1],
+            "short_name": row[2],
+            "created_date": row[3].strftime("%d %b %Y"),
+            "update_date": row[4].strftime("%d %b %Y"),
+            "user": row[5],
+            "signatures": row[6]
+        })
+    cur.close()
+    con.close()
+    return jsonify(entries)
--- pronto/static/js/index.js
+++ pronto/static/js/index.js
@@ -5,11 +5,11 @@ import {updateHeader} from "./ui/header.js"
 function getDatabases() {
     return new Promise(((resolve, reject) => {
         fetch('/api/databases/')
-        .then(response => response.json())
-        .then(databases => {
-            let html = '';
-            for (const database of databases) {
-                html += `
+            .then(response => response.json())
+            .then(databases => {
+                let html = '';
+                for (const database of databases) {
+                    html += `
                     <tr>
                     <td style="border-left: 5px solid ${database.color};" class="collapsing">
                         <a target="_blank" href="${database.link}">${database.name}<i class="external icon"></i></a>
@@ -26,20 +26,51 @@ function getDatabases() {
                     </td>
                     </tr>
                 `;
-            }
-            document.querySelector('.segment[data-tab="databases"] tbody').innerHTML = html;
-            resolve();
-        });
+                }
+                document.querySelector('.segment[data-tab="databases"] tbody').innerHTML = html;
+                resolve();
+            });
     }));
 }
 
 function getRecentEntries() {
     return new Promise(((resolve, reject) => {
-        fetch('/api/entries/')
+        fetch('/api/entries/news/')
+            .then(response => response.json())
+            .then(object => {
+                let html = '';
+                for (const entry of object.entries) {
+                    html += `
+                    <tr>
+                    <td>
+                        <span class="ui circular mini label type ${entry.type}">${entry.type}</span>
+                        <a href="/entry/${entry.accession}/">${entry.accession}</a>
+                    </td>
+                    <td>${entry.short_name}</td>
+                    <td>${entry.signatures}</td>
+                    <td>${checkbox.createDisabled(entry.checked)}</td>
+                    <td>${entry.date}</td>
+                    <td>${entry.user}</td>
+                    </tr>
+                `;
+                }
+
+                const tab = document.querySelector('.segment[data-tab="recent-entries"]');
+                tab.querySelector('tbody').innerHTML = html;
+                tab.querySelector(':scope > p').innerHTML = `<strong>${object.entries.length}</strong> ${object.entries.length > 1 ? 'entries' : 'entry'} created since <strong>${object.date}</strong>.`;
+
+                document.querySelector('.item[data-tab="recent-entries"] .label').innerHTML = object.entries.length.toString();
+                resolve();
+            });
+    }));
+}
+
+function getUncheckedEntries() {
+    return fetch('/api/entries/unchecked/')
         .then(response => response.json())
-        .then(object => {
+        .then(entries => {
             let html = '';
-            for (const entry of object.entries) {
+            for (const entry of entries) {
                 html += `
                     <tr>
                     <td>
@@ -47,35 +78,40 @@ function getRecentEntries() {
                         <a href="/entry/${entry.accession}/">${entry.accession}</a>
                     </td>
                     <td>${entry.short_name}</td>
-                    <td>${entry.date}</td>
-                    <td>${entry.author}</td>
                     <td>${entry.signatures}</td>
-                    <td>${checkbox.createDisabled(entry.checked)}</td>
+                    <td>${entry.created_date}</td>
+                    <td>${entry.update_date}</td>
+                    <td>${entry.user}</td>
                     </tr>
                 `;
             }
 
-            const tab = document.querySelector('.segment[data-tab="entries"]');
+            const tab = document.querySelector('.segment[data-tab="unchecked-entries"]');
             tab.querySelector('tbody').innerHTML = html;
-            tab.querySelector(':scope > p').innerHTML = `<strong>${object.entries.length}</strong> ${object.entries.length > 1 ? 'entries' : 'entry'} created since <strong>${object.date}</strong>.`;
 
-            $(tab.querySelector('.message .close'))
-                .on('click', function() {
-                    $(this)
-                        .closest('.message')
-                        .transition('fade');
-                });
-            resolve();
+            document.querySelector('.item[data-tab="unchecked-entries"] .label').innerHTML = entries.length.toString();
         });
-    }));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     updateHeader();
 
     dimmer.on();
-    Promise.all([getDatabases(), getRecentEntries()])
+    const promises = [
+        getDatabases(),
+        getRecentEntries(),
+        getUncheckedEntries()
+    ];
+
+    Promise.all(promises)
         .then(() => {
+            $(document.querySelectorAll('.message .close'))
+                .on('click', function() {
+                    $(this)
+                        .closest('.message')
+                        .transition('fade');
+                });
+
             $('.tabular.menu .item').tab();
             dimmer.off();
         });
