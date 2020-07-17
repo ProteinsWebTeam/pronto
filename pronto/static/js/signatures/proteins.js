cc2818f3917ca56b7858f1a5ce23b71d7568a63a
Matthias Blum <mat.blum@gmail.com>
2020-07-17 16:09:03 +0100

    Option to not display all matches (only matches for observed signatures)

--- pronto/static/js/signatures/proteins.js
+++ pronto/static/js/signatures/proteins.js
@@ -5,181 +5,178 @@ import {setClass} from "../ui/utils.js";
 import * as pagination from "../ui/pagination.js";
 import {genProtHeader} from "../ui/proteins.js";
 
-function fetchAPI() {
-    return new Promise(((resolve, reject) => {
-        fetch('/api' + location.pathname + location.search)
-            .then(response => {
-                response.json()
-                    .then(object => {
-                        if (response.ok)
-                            resolve(object);
-                        else
-                            reject(object.error);
-                    });
-            })
-    }));
-}
 
 const svgWidth = 700;
 const svgPaddingLeft = 5;
 const svgPaddingRight = 30;
 const width = svgWidth - svgPaddingLeft - svgPaddingRight;
+const copyKey = 'proteinAccessions';
 
 
-function getMatches(proteinAccession, signatures) {
+function fetchMatches(proteinAccession) {
     return fetch(`/api/protein/${proteinAccession}/?matches`)
         .then(response => response.json())
         .then(protein => {
-            let html = `<h4 class="ui header">${genProtHeader(protein)}</h4>
-                        <table class="ui single line very basic compact table"><tbody>`;
-
-            for (const signature of protein.signatures) {
-                if (signatures.includes(signature.accession))
-                    html += '<tr class="active">';
-                else
-                    html += '<tr>';
-
-                if (signature.entry === null)
-                    html += '<td></td>';
-                else {
-                    html += `<td>
-                                   <span class="ui mini circular label type ${signature.entry.type}">${signature.entry.type}</span>
-                                   <a href="/entry/${signature.entry.accession}/">${signature.entry.accession}</a>
-                                 </td>`;
-                }
-
-                html += `<td><a href="/signature/${signature.accession}/">${signature.accession}</a></td>
-                             <td class="collapsing"><a href="#" data-add-id="${signature.accession}"><i class="cart plus fitted icon"></i></a></td>
-                             <td><a target="_blank" href="${signature.link}">${signature.name}<i class="external icon"></i></a></td>
-                             <td>
-                                <svg width="${svgWidth}" height="30">
-                                    <line x1="${svgPaddingLeft}" y1="20" x2="${width}" y2="20" stroke="#888" stroke-width="1px"/>
-                                    <text x="${svgPaddingLeft + width + 2}" y="20" class="length">${protein.length}</text>`;
-
-                for (const fragments of signature.matches) {
-                    for (let i = 0; i < fragments.length; i++) {
-                        const frag = fragments[i];
-                        const x = Math.round(frag.start * width / protein.length) + svgPaddingLeft;
-                        const w = Math.round((frag.end - frag.start) * width / protein.length);
-
-                        html += '<g>';
-                        if (i) {
-                            // Discontinuous domain: draw arc
-                            const px = Math.round(fragments[i-1].end * width / protein.length) + svgPaddingLeft;
-                            html += `<path d="M${px} 15 Q ${(px+x)/2} 0 ${x} 15" fill="none" stroke="${signature.color}"/>`
-                        }
-
-                        html += `<rect x="${x}" y="15" width="${w}" height="10" rx="1" ry="1" style="fill: ${signature.color};" />
-                                     <text x="${x}" y="10" class="position">${frag.start}</text>
-                                     <text x="${x+w}" y="10" class="position">${frag.end}</text>
-                                     </g>`;
-                    }
-                }
-
-                html += '</svg></td></tr>';
-            }
-
-            html += '</tbody></table>';
-            document.querySelector(`#proteins [data-id="${protein.accession}"]`).innerHTML = html;
-            return protein.accession;
+            sessionStorage.setItem(proteinAccession, JSON.stringify(protein));
         });
 }
 
-function getProteins(signatureAccessions) {
+
+function renderMatches(proteinAccession, signatures, filterMatches) {
+    const protein = JSON.parse(sessionStorage.getItem(proteinAccession));
+
+    let html = `<h4 class="ui header">${genProtHeader(protein)}</h4>
+                        <table class="ui single line very basic compact table"><tbody>`;
+
+    for (const signature of protein.signatures) {
+        if (signatures.includes(signature.accession)) {
+            if (filterMatches)
+                html += '<tr>';
+            else
+                html += '<tr class="active">';
+        }
+        else if (filterMatches)
+            continue
+        else
+            html += '<tr>';
+
+        if (signature.entry === null)
+            html += '<td></td>';
+        else {
+            html += `<td>
+                       <span class="ui mini circular label type ${signature.entry.type}">${signature.entry.type}</span>
+                       <a href="/entry/${signature.entry.accession}/">${signature.entry.accession}</a>
+                     </td>`;
+        }
+
+        html += `<td><a href="/signature/${signature.accession}/">${signature.accession}</a></td>
+                 <td class="collapsing"><a href="#" data-add-id="${signature.accession}"><i class="cart plus fitted icon"></i></a></td>
+                 <td><a target="_blank" href="${signature.link}">${signature.name}<i class="external icon"></i></a></td>
+                 <td>
+                    <svg width="${svgWidth}" height="30">
+                        <line x1="${svgPaddingLeft}" y1="20" x2="${width}" y2="20" stroke="#888" stroke-width="1px"/>
+                        <text x="${svgPaddingLeft + width + 2}" y="20" class="length">${protein.length}</text>`;
+
+        for (const fragments of signature.matches) {
+            for (let i = 0; i < fragments.length; i++) {
+                const frag = fragments[i];
+                const x = Math.round(frag.start * width / protein.length) + svgPaddingLeft;
+                const w = Math.round((frag.end - frag.start) * width / protein.length);
+
+                html += '<g>';
+                if (i) {
+                    // Discontinuous domain: draw arc
+                    const px = Math.round(fragments[i-1].end * width / protein.length) + svgPaddingLeft;
+                    html += `<path d="M${px} 15 Q ${(px+x)/2} 0 ${x} 15" fill="none" stroke="${signature.color}"/>`
+                }
+
+                html += `<rect x="${x}" y="15" width="${w}" height="10" rx="1" ry="1" style="fill: ${signature.color};" />
+                         <text x="${x}" y="10" class="position">${frag.start}</text>
+                         <text x="${x+w}" y="10" class="position">${frag.end}</text>
+                         </g>`;
+            }
+        }
+
+        html += '</svg></td></tr>';
+    }
+
+    html += '</tbody></table>';
+    document.querySelector(`#proteins [data-id="${proteinAccession}"]`).innerHTML = html;
+}
+
+
+async function getProteins(signatureAccessions) {
     dimmer.on();
 
-    fetchAPI()
-        .then(
-            (data,) => {
-                let elem = document.querySelector('#count .value');
-                elem.dataset.count = data.count;
-                elem.innerHTML = data.count.toLocaleString();
+    fetch(`/api${location.pathname+location.search}`)
+        .then(response => response.json())
+        .then((data,) => {
+            let elem = document.querySelector('#count .value');
+            elem.dataset.count = data.count;
+            elem.innerHTML = data.count.toLocaleString();
 
-                let html = '';
-                for (const protein of data.results) {
-                    // html += `<div data-id="${protein.accession}" class="ui segment">
-                    //          <h4 class="ui header">${genProtHeader(protein)}</h4>
-                    //          <table class="ui single line very basic compact table"><tbody></tbody></table>
-                    //          </div>`;
-                    html += `<div data-id="${protein}" class="ui segment"></div>`;
-                }
-
-                elem = document.getElementById('proteins');
-                elem.innerHTML = html;
-
-                pagination.render(
-                    elem.parentNode,
-                    data.page_info.page,
-                    data.page_info.page_size,
-                    data.count,
-                    (url,) => {
-                        history.replaceState(null, document.title, url);
-                        getProteins(signatureAccessions);
-                    }
-                );
-
-                let filterElem;
-                filterElem = document.querySelector('[data-filter="comment"]');
-                if (data.filters.comment) {
-                    filterElem.querySelector('.value').innerHTML = `${data.filters.comment}`;
-                    setClass(filterElem, 'hidden', false);
-
-                } else
-                    setClass(filterElem, 'hidden', true);
-
-                filterElem = document.querySelector('[data-filter="description"]');
-                if (data.filters.description) {
-                    filterElem.querySelector('.value').innerHTML = `${data.filters.description}`;
-                    setClass(filterElem, 'hidden', false);
-
-                } else
-                    setClass(filterElem, 'hidden', true);
-
-                filterElem = document.querySelector('[data-filter="go"]');
-                if (data.filters.go) {
-                    filterElem.querySelector('.value').innerHTML = `${data.filters.go}`;
-                    setClass(filterElem, 'hidden', false);
-
-                } else
-                    setClass(filterElem, 'hidden', true);
-
-                filterElem = document.querySelector('[data-filter="exclude"]');
-                if (data.filters.exclude.length) {
-                    filterElem.querySelector('.value').innerHTML = data.filters.exclude.map(acc => `<span class="ui small basic label">${acc}</span>`).join('');
-                    setClass(filterElem, 'hidden', false);
-                } else
-                    setClass(filterElem, 'hidden', true);
-
-                filterElem = document.querySelector('[data-filter="taxon"]');
-                if (data.filters.taxon) {
-                    filterElem.querySelector('.value').innerHTML = `<em>${data.filters.taxon}</em>`;
-                    setClass(filterElem, 'hidden', false);
-                } else
-                    setClass(filterElem, 'hidden', true);
-
-                const promises = [];
-                for (const protein of data.results) {
-                    //promises.push(getMatches(protein.accession, signatureAccessions));
-                    promises.push(getMatches(protein, signatureAccessions));
-                }
-
-                Promise.all(promises).then(() => {
-                    for (const elem of document.querySelectorAll('a[data-add-id]')) {
-                        elem.addEventListener('click', e => {
-                            e.preventDefault();
-                            selector.add(elem.dataset.addId).render();
-                        });
-                    }
-
-                    dimmer.off()
-                });
-
-            },
-            (error,) => {
-                dimmer.off();
-                console.error(error);  // todo
+            let html = '';
+            for (const protein of data.results) {
+                html += `<div data-id="${protein}" class="ui segment"></div>`;
             }
-        );
+
+            elem = document.getElementById('proteins');
+            elem.innerHTML = html;
+
+            pagination.render(
+                elem.parentNode,
+                data.page_info.page,
+                data.page_info.page_size,
+                data.count,
+                (url,) => {
+                    history.replaceState(null, document.title, url);
+                    getProteins(signatureAccessions);
+                }
+            );
+
+            let filterElem;
+            filterElem = document.querySelector('[data-filter="comment"]');
+            if (data.filters.comment) {
+                filterElem.querySelector('.value').innerHTML = `${data.filters.comment}`;
+                setClass(filterElem, 'hidden', false);
+
+            } else
+                setClass(filterElem, 'hidden', true);
+
+            filterElem = document.querySelector('[data-filter="description"]');
+            if (data.filters.description) {
+                filterElem.querySelector('.value').innerHTML = `${data.filters.description}`;
+                setClass(filterElem, 'hidden', false);
+
+            } else
+                setClass(filterElem, 'hidden', true);
+
+            filterElem = document.querySelector('[data-filter="go"]');
+            if (data.filters.go) {
+                filterElem.querySelector('.value').innerHTML = `${data.filters.go}`;
+                setClass(filterElem, 'hidden', false);
+
+            } else
+                setClass(filterElem, 'hidden', true);
+
+            filterElem = document.querySelector('[data-filter="exclude"]');
+            if (data.filters.exclude.length) {
+                filterElem.querySelector('.value').innerHTML = data.filters.exclude.map(acc => `<span class="ui small basic label">${acc}</span>`).join('');
+                setClass(filterElem, 'hidden', false);
+            } else
+                setClass(filterElem, 'hidden', true);
+
+            filterElem = document.querySelector('[data-filter="taxon"]');
+            if (data.filters.taxon) {
+                filterElem.querySelector('.value').innerHTML = `<em>${data.filters.taxon}</em>`;
+                setClass(filterElem, 'hidden', false);
+            } else
+                setClass(filterElem, 'hidden', true);
+
+            // Remove all stored data (except accessions for the 'copy' button)
+            for (let i = 0; i < sessionStorage.length; i++) {
+                const key = sessionStorage.key(i);
+                if (key !== copyKey)
+                    sessionStorage.removeItem(key);
+            }
+
+            const filterMatches = document.querySelector('input[name="filter-matches"]').checked;
+            const promises = [];
+            for (const protein of data.results) {
+                promises.push(fetchMatches(protein).then(() => renderMatches(protein, signatureAccessions, filterMatches)));
+            }
+
+            Promise.all(promises).then(() => {
+                for (const elem of document.querySelectorAll('a[data-add-id]')) {
+                    elem.addEventListener('click', e => {
+                        e.preventDefault();
+                        selector.add(elem.dataset.addId).render();
+                    });
+                }
+
+                dimmer.off()
+            });
+        });
 }
 
 
@@ -195,7 +192,7 @@ document.addEventListener('DOMContentLoaded', () => {
     selector.render().tab("proteins");
 
     const url = new URL(location.href);
-    const checkbox = document.querySelector('input[type=checkbox][name=reviewed]');
+    let checkbox = document.querySelector('input[type=checkbox][name=reviewed]');
     checkbox.checked = url.searchParams.has('reviewed');
     checkbox.addEventListener('change', e => {
         if (e.currentTarget.checked)
@@ -215,6 +212,16 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('download-btn').addEventListener('click', e => {
         e.preventDefault();
         downloadProteins();
+    });
+
+    checkbox = document.querySelector('input[type=checkbox][name=filter-matches]');
+    checkbox.checked = url.searchParams.has('filtermatches')
+    checkbox.addEventListener('change', e => {
+        e.preventDefault();
+        const filterMatches = e.currentTarget.checked;
+        for (const elem of document.querySelectorAll('#proteins [data-id]')) {
+            renderMatches(elem.dataset.id, accessions, filterMatches);
+        }
     });
 
     updateHeader();
@@ -242,18 +249,21 @@ function copyProteins() {
         }
     };
 
-    if (btn.dataset.value !== undefined && btn.dataset.value.length > 0) {
-        copy2clipboard(btn.dataset.value);
+    const value = sessionStorage.getItem(copyKey);
+    if (value !== null) {
+        copy2clipboard(value);
         return;
     }
 
+    btn.className = 'ui disabled loading tertiary button';
     const url = new URL(location.href);
     url.searchParams.set('page_size', '0');
     fetch(`/api${url.pathname}${url.search}`)
         .then(response => response.json())
         .then(object => {
-            btn.dataset.value = object.results.join(' ');
-            copy2clipboard(btn.dataset.value);
+            const value = object.results.join(' ');
+            sessionStorage.setItem(copyKey, value);
+            copy2clipboard(value);
         });
 }
 
