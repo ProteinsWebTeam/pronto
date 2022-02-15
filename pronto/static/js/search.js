796095db8078a1237d31e8b00549693d57688461
Matthias Blum <mat.blum@gmail.com>
2022-02-15 18:33:23 +0000

    Fix page rendering if EBISearch returns an error

--- pronto/static/js/search.js
+++ pronto/static/js/search.js
@@ -3,26 +3,30 @@ import {render} from "./ui/pagination.js"
 import * as dimmer from "./ui/dimmer.js"
 
 
-function callEBISearch(query, page, pageSize) {
+async function callEBISearch(query, page, pageSize) {
     const url = new URL("https://www.ebi.ac.uk/ebisearch/ws/rest/interpro7");
     url.searchParams.set("query", query);
     url.searchParams.set("format", "json");
     url.searchParams.set("fields", "type,name,source_database");
-    url.searchParams.set("start", (page-1)*pageSize);
-    url.searchParams.set("size", pageSize);
+    url.searchParams.set("start", ((page - 1) * pageSize).toString());
+    url.searchParams.set("size", pageSize.toString());
 
     dimmer.on();
-    fetch(url.toString())
-        .then(response => response.json())
-        .then(result => {
-            let html = "";
-            if (result.hitCount) {
-                result.entries.forEach(entry => {
-                    const accession = entry.id;
-                    const type = entry.fields.type[0].charAt(0).toUpperCase();
-                    const database = entry.fields.source_database[0];
-                    const name = entry.fields.name[0];
-                    html += `<tr>
+    const response = await fetch(url.toString());
+    dimmer.off();
+
+    let html = "";
+    let hitCount = 0;
+    if (response.ok) {
+        const result = await response.json();
+        hitCount = result.hitCount;
+        if (hitCount) {
+            result.entries.forEach(entry => {
+                const accession = entry.id;
+                const type = entry.fields.type[0].charAt(0).toUpperCase();
+                const database = entry.fields.source_database[0];
+                const name = entry.fields.name[0];
+                html += `<tr>
                                <td>
                                  <span class="ui tiny circular label type ${type}">${type}</span>
                                  <a href="/${database.toLowerCase() === 'interpro' ? 'entry' : 'signature'}/${accession}/">${accession}</a>
@@ -30,26 +34,26 @@ function callEBISearch(query, page, pageSize) {
                                <td>${database}</td>
                                <td>${name}</td>
                              </tr>`;
-                });
-            } else {
-                html = `<tr><td colspan="3" class="center aligned">No results found for <strong>${query}</strong></td></tr>`;
-            }
+            });
+        }
+    }
 
-            const table = document.querySelector("#results");
-            table.querySelector("tbody").innerHTML = html;
-            render(
-                table,
-                page,
-                pageSize,
-                result.hitCount,
-                (newURL,) => {
-                    const params = new URL(newURL).searchParams;
-                    const newPage = Number(params.get("page"));
-                    callEBISearch(query, newPage, pageSize);
-                }
-            );
-            dimmer.off();
-        });
+    if (html.length === 0)
+        html = `<tr><td colspan="3" class="center aligned">No results found for <strong>${query}</strong></td></tr>`;
+
+    const table = document.querySelector("#results");
+    table.querySelector("tbody").innerHTML = html;
+    render(
+        table,
+        page,
+        pageSize,
+        hitCount,
+        (newURL,) => {
+            const params = new URL(newURL).searchParams;
+            const newPage = Number(params.get("page"));
+            callEBISearch(query, newPage, pageSize);
+        }
+    );
 }
 
 
