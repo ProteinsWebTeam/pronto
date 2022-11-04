4328b6c04c75615e52dd300aa89d39c196a2247a
Matthias Blum <mat.blum@gmail.com>
2022-11-04 19:08:18 +0000

    TIGRfams -> NCBIfam transition

--- pronto/utils.py
+++ pronto/utils.py
@@ -23,12 +23,13 @@ SIGNATURES = [
     r"SFLD[FGS]\d{4,}",     # SFLD
     r"SM\d{4,}",            # SMART
     r"SSF\d{4,}",           # SUPERFAMILY
-    r"TIGR\d{4,}",          # TIGRFAMs
+    r"NF\d{6}",             # NCBIfam
+    r"TIGR\d{4,}",          # NCBIfam
     r"cd\d{4,}",            # CDD
     r"sd\d{4,}"             # CDD
 ]
 
-
+# Keys need to be in lower case!
 XREFS = {
     "CATHGENE3D": "http://www.cathdb.info/superfamily/{}",
     "CAZY": "http://www.cazy.org/fam/{}.html",
@@ -47,7 +48,7 @@ XREFS = {
     "SSF": "http://supfam.org/SUPERFAMILY/cgi-bin/scop.cgi?ipid={}",
     "SUPERFAMILY": "http://supfam.org/SUPERFAMILY/cgi-bin/scop.cgi?ipid={}",
     "SWISSPROT": "https://www.uniprot.org/uniprotkb/{}/entry",
-    "TIGRFAMS": "https://www.ncbi.nlm.nih.gov/genome/annotation_prok/evidence/{}/"
+    "NCBIFAM": "https://www.ncbi.nlm.nih.gov/genome/annotation_prok/evidence/{}/"
 }
 
 
@@ -388,12 +389,12 @@ class Superfamily:
         return self.home + '/cgi-bin/scop.cgi?ipid=' + acc
 
 
-class Tigrfams:
-    home = "https://www.ncbi.nlm.nih.gov/genome/annotation_prok/tigrfams/"
+class NCBIfam:
+    home = "https://www.ncbi.nlm.nih.gov/protfam/"
     color = '#a04867'
 
     def gen_link(self, acc: str):
-        return f"https://www.ncbi.nlm.nih.gov/genome/annotation_prok/evidence/{acc}/"
+        return XREFS["NCBIFAM"].format(acc)
 
 
 def get_database_obj(key: str):
@@ -405,6 +406,7 @@ def get_database_obj(key: str):
         "hamap": Hamap,
         "mobidblt": MobiDbLite,
         "mobidb lite": MobiDbLite,
+        "ncbifam": NCBIfam,
         "panther": Panther,
         "pfam": Pfam,
         "pirsf": Pirsf,
@@ -417,7 +419,6 @@ def get_database_obj(key: str):
         "smart": Smart,
         "ssf": Superfamily,
         "superfamily": Superfamily,
-        "tigrfams": Tigrfams,
     }
 
     try:
