9814f223dfcfe7ec7c3d61e38c6ae8ce4a888186
Matthias Blum <mat.blum@gmail.com>
2023-07-10 17:00:16 +0100

    Add /api/activity/ endpoint

--- pronto/api/__init__.py
+++ pronto/api/__init__.py
@@ -49,6 +49,58 @@ def api_index():
     })
 
 
+@bp.route("/activity/")
+def get_activity():
+    seconds = int(request.args.get('s', 24 * 3600))
+
+    con = utils.connect_oracle()
+    cur = con.cursor()
+    cur.execute(
+        f"""
+        SELECT A.TYPE, A.PRIMARY_ID, A.SECONDARY_ID, NVL(U.NAME, A.DBUSER), 
+               A.ACTION, A.TIMESTAMP
+        FROM (
+            SELECT 'CA' AS TYPE, ANN_ID AS PRIMARY_ID, NULL AS SECONDARY_ID, 
+                   DBUSER, ACTION, TIMESTAMP
+            FROM INTERPRO.COMMON_ANNOTATION_AUDIT
+            UNION ALL
+            SELECT 'E2C', ENTRY_AC, ANN_ID, DBUSER, ACTION, TIMESTAMP
+            FROM INTERPRO.ENTRY2COMMON_AUDIT
+            UNION ALL
+            SELECT 'E2E', ENTRY_AC, PARENT_AC, DBUSER, ACTION, TIMESTAMP
+            FROM INTERPRO.ENTRY2ENTRY_AUDIT
+            UNION ALL
+            SELECT 'E2M', ENTRY_AC, METHOD_AC, DBUSER, ACTION, TIMESTAMP
+            FROM INTERPRO.ENTRY2METHOD_AUDIT
+            UNION ALL
+            SELECT 'E', ENTRY_AC, NULL, DBUSER, ACTION, TIMESTAMP
+            FROM INTERPRO.ENTRY_AUDIT
+            UNION ALL
+            SELECT 'I2G', ENTRY_AC, GO_ID, DBUSER, ACTION, TIMESTAMP
+            FROM INTERPRO.INTERPRO2GO_AUDIT
+        ) A
+        LEFT OUTER JOIN INTERPRO.PRONTO_USER U ON A.DBUSER = U.DB_USER
+        WHERE A.TIMESTAMP >= SYSDATE - INTERVAL '{seconds}' SECOND
+        ORDER BY A.TIMESTAMP DESC
+        """
+    )
+
+    activity = []
+    for t, id1, id2, user, action, ts in cur.fetchall():
+        activity.append({
+            "type": t,
+            "primary_id": id1,
+            "secondary_id": id2,
+            "action": action,
+            "timestamp": ts.strftime("%d %b %Y at %H:%M")
+        })
+
+    cur.close()
+    con.close()
+
+    return jsonify(activity)
+
+
 @bp.route("/tasks/")
 def get_tasks():
     return jsonify(utils.executor.get_tasks(
