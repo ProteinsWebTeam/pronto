2cfd496f3bbafc8b58d22caccd3904afcd0d111e
typhainepl <typhaine@ebi.ac.uk>
2024-01-23 16:14:33 +0000

    changes following PR comment

--- pronto/utils.py
+++ pronto/utils.py
@@ -33,7 +33,7 @@ XREFS = {
     "CATHGENE3D": "http://www.cathdb.info/superfamily/{}",
     "CAZY": "http://www.cazy.org/{}.html",
     "COG": "https://ftp.ncbi.nih.gov/pub/COG/COG2014/static/byCOG/{}.html",
-    "ENZYME": "https://enzyme.expasy.org/EC/{}",
+    "EC": "https://enzyme.expasy.org/EC/{}",
     "GENPROP": "https://www.ebi.ac.uk/interpro/genomeproperties/#{}",
     "INTERPRO": "/entry/{}",
     "MIM": "https://www.omim.org/entry/{}",
