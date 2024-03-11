f08c0abcdc8a11be9b9eaaa6a61c9180a8b53b70
Emma Elizabeth Mary Hobbs <ehobbs@ubu-n-b6c04f563034.ebi.ac.uk>
2024-03-11 17:20:23 +0000

    add llm entry check

--- scripts/create_ai_entries.py
+++ scripts/create_ai_entries.py
@@ -45,25 +45,38 @@ def main(argv: Optional[List[str]] = None):
 
     s = requests.Session()
     r = s.post(
-        f"{args.pronto}/login/".replace("//login", "/login"),
+        f"{args.pronto}/login/",
         data=payload,
         timeout=args.timeout,
     )
 
-    if (s.get(f"{args.pronto}/api/".replace("//api", "/api")).text).find('"user": Null') != -1:
+    if s.cookies.get("session") is None:
         logger.error(
             (
                 "Failed to log into pronto.\n"
                 "Check the url (%s), username and password are correct\n"
                 "Terminating program"
             ),
-            "http://localhost:5000/login/"
+            f"{args.pronto}/login/"
         )
         sys.exit(1)
     else:
         logger.warning("Logged in as %s", args.username)
 
     create_entries(s, args)
+
+
+def is_llm(signature: dict) -> bool:
+    keys = ["llm_name", "llm_description", "llm_abstract"]
+    return any(signature.get(key) for key in keys)
+
+
+def write_error(message, args):
+    if args.cache:
+        with open("failed_signatures.tsv", "a") as fh:
+            fh.write(message)
+    else:
+        sys.stderr.write(message)
 
 
 def create_entries(
@@ -99,6 +112,8 @@ def create_entries(
             "50%": round(sig_total * 0.5),
             "75%": round(sig_total * 0.75),
         }
+        sig_perc = {f"{i*10}%": round(sig_total * i/20) for i in range(1, 21)}
+
         for signature in sigs_query.json()['results']:
             perc = (sig_count / sig_total * 100)
             if perc in sig_perc.values():
@@ -113,6 +128,37 @@ def create_entries(
             if sig_response.status_code != 200:
                 logger.error("Could not retrieve data for signature %s", sig_acc)
                 continue
+
+            if is_llm(signature) is False:
+                print("F")
+                try:
+                    signature['llm_name']
+                except KeyError:
+                    print("not LLM NAME")
+                try:
+                    signature['llm_description']
+                except KeyError:
+                    print("not LLM description")
+                try:
+                    print(signature['llm_abstract'])
+                except KeyError:
+                    print("Not LLM ABSTRACT")
+                continue
+            print("TRUE********")
+
+            if sig_response.json()['llm_description'] is not None:
+                if len(sig_response.json()['llm_description']) > 30:
+                    logger.error(
+                        (
+                            "Short name '%s' too long. Entry short names cannot be longer than 30 characters.\n"
+                        ),
+                        sig_response.json()['llm_description']
+                    )
+                    message = (
+                        f"SIGNATURE:{sig_acc}\t{sig_response.json()['llm_name']}\t{sig_response.json()['llm_description']}\t"
+                        f"ERROR: Short name too long.\n"
+                    )
+                    write_error(message, args)
 
             payload = {
                 "type": sig_type,
@@ -155,11 +201,7 @@ def create_entries(
                             f"SIGNATURE:{sig_acc}\t{sig_response.json()['llm_name']}\t{sig_response.json()['llm_description']}\t"
                             f"ENTRY:{entry['accession']}\t{entry['name']}\t{entry['short_name']}\n"
                         )
-                        if args.cache:
-                            with open("failed_signatures.tsv", "a") as fh:
-                                fh.write(message)
-                        else:
-                            sys.stderr.write(message)
+                        write_error(message, args)
                 else:
                     logger.error(
                         "Error (HTTP code: %s) raised while tying to generate entry for signature %s:\n%s",
