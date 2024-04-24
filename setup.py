d390802ac72aec83666d728c52ccad4419394365
Matthias Blum <mat.blum@gmail.com>
2024-04-24 21:57:28 +0100

    Remove __version__

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ import os
 from setuptools import setup, find_packages
 
 
-def get_version():
-    path = os.path.join(os.path.dirname(__file__), "pronto", "__init__.py")
-    with open(path, "rt") as fh:
-        for line in fh:
-            if line.startswith("__version__"):
-                return line.split('=', 1)[1].strip().replace('"', '')
-
-
 def get_requires():
     path = os.path.join(os.path.dirname(__file__), "requirements.txt")
     with open(path, "rt") as fh:
@@ -21,7 +13,7 @@ def get_requires():
 
 setup(
     name="pronto",
-    version=get_version(),
+    version="2.7.1",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
