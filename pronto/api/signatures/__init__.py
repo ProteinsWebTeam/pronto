f703925f5bc3aa987a2b69674dbb77ef722b57f7
Matthias Blum <mat.blum@gmail.com>
2025-10-18 11:12:03 +0100

    Change default TrEMBL similarity threshold

--- pronto/api/signatures/__init__.py
+++ pronto/api/signatures/__init__.py
@@ -36,7 +36,7 @@ from . import taxonomy
 @bp.route("/unintegrated/similar/")
 def get_similar_unintegrated():
     min_sprot = float(request.args.get("min-sprot", 1))
-    min_trembl = float(request.args.get("min-trembl", 0.25))
+    min_trembl = float(request.args.get("min-trembl", 0.9))
     database = request.args.get("database")
 
     try:
