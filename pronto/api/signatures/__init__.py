4890830e701f771ab438933297beff3a1f5df9de
Matthias Blum <mat.blum@gmail.com>
2025-10-18 02:41:09 +0100

    New similar signatures page

--- pronto/api/signatures/__init__.py
+++ pronto/api/signatures/__init__.py
@@ -35,6 +35,228 @@ from . import taxonomy
 
 @bp.route("/unintegrated/similar/")
 def get_similar_unintegrated():
+    min_sprot = float(request.args.get("min-sprot", 1))
+    min_trembl = float(request.args.get("min-trembl", 0.25))
+    database = request.args.get("database")
+
+    try:
+        page = int(request.args["page"])
+    except (KeyError, ValueError):
+        page = 1
+
+    try:
+        page_size = int(request.args["page_size"])
+    except (KeyError, ValueError):
+        page_size = 20
+
+    con = utils.connect_oracle()
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT EM.METHOD_AC, E.ENTRY_AC, E.ENTRY_TYPE, E.NAME, E.SHORT_NAME, E.CHECKED
+        FROM INTERPRO.ENTRY2METHOD EM
+        INNER JOIN INTERPRO.ENTRY E ON EM.ENTRY_AC = E.ENTRY_AC
+        """
+    )
+    integrated = {}
+    for row in cur:
+        integrated[row[0]] = {
+            "accession": row[1],
+            "type": row[2],
+            "name": row[3],
+            "short_name": row[4],
+            "checked": row[5] == 'Y'
+        }
+
+    cur.execute(
+        """
+        SELECT METHOD_AC, COUNT(*)
+        FROM INTERPRO.METHOD_COMMENT
+        WHERE STATUS = 'Y'
+        GROUP BY METHOD_AC
+        """
+    )
+    num_comments = dict(cur.fetchall())
+    cur.close()
+    con.close()
+
+    con = utils.connect_pg()
+    cur = con.cursor()
+    try:
+        filters = [
+            "s1.num_complete_sequences > 0",
+            "s2.num_complete_sequences > 0",
+        ]
+        params = []
+
+        database_id = None
+        if database:
+            cur.execute(
+                """
+                SELECT id, name
+                FROM interpro.database
+                WHERE LOWER(name) = %s
+                """,
+                [database.lower()]
+            )
+            row = cur.fetchone()
+            if not row:
+                return (
+                    jsonify({
+                        "error": {
+                            "title": "Bad Request",
+                            "message": f"Invalid database parameter: {database}",
+                        }
+                    }),
+                    400
+                )
+
+            database_id, database = row
+
+        if database_id is not None:
+            filters.append("d1.id = %s")
+            params.append(database_id)
+
+        cur.execute(
+            f"""
+            SELECT *
+            FROM (
+                SELECT x.accession1, x.name1, x.description1,
+                       x.db_id1, x.db_name1,
+                       x.sprot1, x.trembl1,
+                       x.accession2, x.name2, x.description2,
+                       x.db_id2, x.db_name2,
+                       x.sprot2, x.trembl2,
+                       x.ovl_sprot,
+                       COALESCE(x.ovl_sprot::float / NULLIF(x.sprot1 + x.sprot2 - x.ovl_sprot, 0), 1) AS f_ovl_sprot,
+                       x.ovl_trembl,
+                       COALESCE(x.ovl_trembl::float / NULLIF(x.trembl1 + x.trembl2 - x.ovl_trembl, 0), 1) AS f_ovl_trembl
+                FROM (
+                  SELECT
+                    s1.accession AS accession1,
+                    s1.name AS name1,
+                    s1.description AS description1,
+                    d1.name AS db_id1,
+                    d1.name_long AS db_name1,
+                    s1.num_complete_reviewed_sequences AS sprot1,
+                    (s1.num_complete_sequences - s1.num_complete_reviewed_sequences) AS trembl1,
+                    s2.accession AS accession2,
+                    s2.name AS name2,
+                    s2.description AS description2,
+                    d2.name AS db_id2,
+                    d2.name_long AS db_name2,
+                    s2.num_complete_reviewed_sequences AS sprot2,
+                    (s2.num_complete_sequences - s2.num_complete_reviewed_sequences) AS trembl2,
+                    c.num_reviewed_80pc_overlaps AS ovl_sprot,
+                    (c.num_80pc_overlaps - c.num_reviewed_80pc_overlaps) AS ovl_trembl
+                  FROM interpro.signature s1
+                  INNER JOIN interpro.comparison c ON s1.accession = c.signature_acc_1
+                  INNER JOIN interpro.signature s2 ON c.signature_acc_2 = s2.accession
+                  INNER JOIN interpro.database d1 ON s1.database_id = d1.id
+                  INNER JOIN interpro.database d2 ON s2.database_id = d2.id
+                  WHERE s1.database_id != s2.database_id
+                  AND (
+                    (s1.type = 'Homologous_superfamily' AND s2.type = 'Homologous_superfamily')
+                    OR
+                    (s1.type != 'Homologous_superfamily' AND s2.type != 'Homologous_superfamily')
+                  )
+                  AND {' AND '.join(filters)}
+                ) x
+            ) x
+            WHERE f_ovl_sprot >= %s
+              AND f_ovl_trembl >= %s
+            """,
+            params + [min_sprot, min_trembl]
+        )
+
+        pthr_sf = re.compile(r"PTHR\d+:SF\d+")
+        candidates = {}
+        for row in cur:
+            if row[0] in integrated or pthr_sf.match(row[0]):
+                # Exclude candidates that are integrated or PANTHER subfams
+                continue
+
+            entry = integrated.get(row[7])
+            if not entry:
+                # Target is not integrated: skip it
+                continue
+
+            try:
+                obj = candidates[row[0]]
+            except KeyError:
+                obj = candidates[row[0]] = {
+                    "accession": row[0],
+                    "name": row[1],
+                    "description": row[2],
+                    "database": {
+                        "color": utils.get_database_obj(row[3]).color,
+                        "name": row[4]
+                    },
+                    "proteins": {
+                        "reviewed": row[5],
+                        "unreviewed": row[6]
+                    },
+                    "comments": num_comments.get(row[0], 0),
+                    "targets": []
+                }
+
+            obj["targets"].append({
+                "accession": row[7],
+                "name": row[8],
+                "description": row[9],
+                "database": {
+                    "color": utils.get_database_obj(row[10]).color,
+                    "name": row[11]
+                },
+                "entry": entry,
+                "proteins": {
+                    "reviewed": row[12],
+                    "unreviewed": row[13],
+                    "overlapping": {
+                        "reviewed": row[14],
+                        "fraction_reviewed": row[15],
+                        "unreviewed": row[16],
+                        "fraction_unreviewed": row[17],
+                    }
+                },
+            })
+    finally:
+        cur.close()
+        con.close()
+
+    results = []
+    for c in candidates.values():
+        c["targets"].sort(key=lambda t: (
+            -t["proteins"]["overlapping"]["fraction_reviewed"],
+            -t["proteins"]["overlapping"]["fraction_unreviewed"],
+            -t["proteins"]["overlapping"]["reviewed"],
+            -t["proteins"]["overlapping"]["unreviewed"],
+        ))
+        results.append(c)
+
+    results.sort(key=lambda c: (
+        -c["targets"][0]["proteins"]["overlapping"]["fraction_reviewed"],
+        -c["targets"][0]["proteins"]["overlapping"]["fraction_unreviewed"],
+        -c["targets"][0]["proteins"]["overlapping"]["reviewed"],
+        -c["targets"][0]["proteins"]["overlapping"]["unreviewed"],
+    ))
+
+    return jsonify({
+        "count": len(results),
+        "results": results[(page-1)*page_size:page*page_size],
+        "page_info": {
+            "page": page,
+            "page_size": page_size
+        },
+        "filters": {
+            "min-sprot": min_sprot,
+            "min-trembl": min_trembl,
+            "database": database
+        },
+    })
+
+
+def get_very_similar_unintegrated():
     min_sim = float(request.args.get("minsim", 0.9))
 
     try:
@@ -207,8 +429,8 @@ def get_specific_unintegrated():
     con = utils.connect_pg()
     cur = con.cursor()
     try:
-        filters = ["s.num_complete_sequences > %s"]
-        params = [0]
+        filters = ["s.num_complete_sequences > 0"]
+        params = []
 
         if with_annotations is True:
             filters += [
--- pronto/static/js/signatures/unintegrated-similar.js
+++ pronto/static/js/signatures/unintegrated-similar.js
@@ -5,57 +5,107 @@ import {initPopups, createPopup, renderCommentLabel} from "../ui/comments.js";
 import {render} from "../ui/pagination.js";
 
 function renderEntry(entry) {
-    if (entry === null)
-        return '<td colspan="2"></td>';
-
     return `
         <td>
             <span class="ui circular mini label type ${entry.type}">${entry.type}</span>
-            <a href="/entry/${entry.accession}/">${entry.accession} (${entry.name})</a>
+            <a href="/entry/${entry.accession}/">${entry.accession} &ndash; ${entry.short_name}</a>
         </td>
         <td class="collapsing">${createDisabled(entry.checked)}</td>
     `;
+}
+
+function renderSignatureLink(s) {
+    let html = `<span class="ui empty circular label" style="background-color: ${s.database.color};" data-content="${s.database.name}" data-position="left center" data-variation="tiny"></span>`
+    if (s.name !== null)
+        html += `<a href="/signature/${s.accession}/">${s.accession} &ndash; ${s.name}</a>`;
+    else
+        html += `<a href="/signature/${s.accession}/">${s.accession}</a>`;
+    return html;
+}
+
+function renderProteinsLink(accession, numProteins, isReviewed) {
+    if (numProteins === 0) return 0;
+    return `<a href="/signatures/${accession}/proteins?reviewed=${isReviewed ? 'true' : 'false'}">
+                ${numProteins.toLocaleString()}
+            </a>`;
 }
 
 async function refresh() {
     dimmer.on();
     const response = await fetch(`/api/signatures/unintegrated/similar/${location.search}`);
     const data = await response.json();
+
+    if (!response.ok) {
+        document.getElementById('results').innerHTML = `
+            <div class="ui error message">
+                <div class="header">${data.error.title}</div>
+                ${data.error.message}
+            </div>
+        `;
+        dimmer.off();
+        return;
+    }
+
     let html = `
-        <table class="ui small celled very compact table">
+        <strong>Minimum similarity:</strong>
+        <div class="ui sib label">
+          Swiss-Prot
+          <div class="detail">&ge; ${(data.filters['min-sprot'] * 100).toFixed(0)}%</div>
+        </div>
+        <div class="ui uniprot label">
+          TrEMBL
+          <div class="detail">&ge; ${(data.filters['min-trembl'] * 100).toFixed(0)}%</div>
+        </div>        
+        <table class="ui celled structured small compact table">
         <thead>
-            <tr><th colspan="6"><div class="ui secondary menu"><span class="item"></span></div></th></tr>
+            <tr><th colspan="11"><div class="ui secondary menu"><span class="item"></span></div></th></tr>
             <tr>
-                <th colspan="2" class="center aligned">Query</th>
-                <th class="center aligned">Target</th>
-                <th colspan="2" class="center aligned">Entry</th>
-                <th class="center aligned">Similarity</th>
+                <th colspan="4">Candidate</th>
+                <th colspan="5">Target</th>
+                <th colspan="2" class="center aligned">Similarity</th>
             </tr>
+                <th colspan="2"></th>
+                <th class="center aligned">Swiss-Prot</th>
+                <th class="center aligned">TrEMBL</th>
+                <th></th>
+                <th class="center aligned">Swiss-Prot</th>
+                <th class="center aligned">TrEMBL</th>    
+                <th colspan="2" class="center aligned">Entry</th>
+                <th class="center aligned">Swiss-Prot</th>
+                <th class="center aligned">TrEMBL</th>
         </thead>
         <tbody>
     `;
+
     for (const obj of data.results) {
+        const rowSpan = obj.targets.length;
         html += `
             <tr>
-            <td>
-                <span class="ui empty circular label" style="background-color: ${obj.query.database.color};" data-content="${obj.query.database.name}" data-position="left center" data-variation="tiny"></span>
-                <a href="/signature/${obj.query.accession}/">${obj.query.accession}</a>
-            </td>
-            <td class="collapsing">${renderCommentLabel(obj.query)}</td>
-            <td>
-                <span class="ui empty circular label" style="background-color: ${obj.target.database.color};" data-content="${obj.target.database.name}" data-position="left center" data-variation="tiny"></span>
-                <a href="/signature/${obj.target.accession}/">${obj.target.accession}</a>
-            </td>
-            ${renderEntry(obj.target.entry)}
-            <td class="right aligned">${obj.similarity.toFixed(4)}</td>
-            </tr>
+            <td rowspan="${rowSpan}">${renderSignatureLink(obj)}</td>
+            <td rowspan="${rowSpan}" class="collapsing">${renderCommentLabel(obj)}</td>
+            <td rowspan="${rowSpan}" class="right aligned">${renderProteinsLink(obj.accession, obj.proteins.reviewed, true)}</td>
+            <td rowspan="${rowSpan}" class="right aligned">${renderProteinsLink(obj.accession, obj.proteins.unreviewed, false)}</td>
         `;
+
+        for (let i = 0; i < obj.targets.length; i++) {
+            const target = obj.targets[i];
+            if (i > 0) html += '<tr>'
+            html += `
+                <td>${renderSignatureLink(target)}</td>
+                <td class="right aligned">${renderProteinsLink(target.accession, target.proteins.reviewed, true)}</td>
+                <td class="right aligned">${renderProteinsLink(target.accession, target.proteins.unreviewed, false)}</td>
+                ${renderEntry(target.entry)}
+                <td class="right aligned">${(target.proteins.overlapping.fraction_reviewed * 100).toFixed(1)}%</td>
+                <td class="right aligned">${(target.proteins.overlapping.fraction_unreviewed * 100).toFixed(1)}%</td>
+                </tr>                
+            `;
+        }
     }
 
     html += `
         </tbody>
         <tfoot>
-        <tr><th colspan="6"><div class="ui right floated pagination menu"></div></th></tr>
+        <tr><th colspan="11"><div class="ui right floated pagination menu"></div></th></tr>
         </tfoot>
     `;
 
@@ -89,44 +139,23 @@ async function refresh() {
 
 document.addEventListener('DOMContentLoaded', () => {
     updateHeader();
-    document.title = 'Highly similar signatures | Pronto';
-    document.querySelector('h1.ui.header').innerHTML = 'Highly similar signatures';
+    document.title = 'Similar signatures | Pronto';
+    document.querySelector('h1.ui.header').innerHTML = 'Similar signatures';
 
     const params = document.getElementById('params');
     params.innerHTML = `
         <p class="justified aligned">
-            This page presents pairs of similar signatures, that is signatures whose matches significantly overlap. The similarity between two signatures is determined by evaluating how many residues in UniProtKB are matched by both signatures.<br>Pairs of signatures where both signatures are integrated are not shown.
-            
+            This page lists unintegrated signatures that are potential candidates 
+            for integration into existing InterPro entries.
+            Candidates are identified based on their similarity to at least 
+            one integrated signature from another member database.<br>
+            Similarity between two signatures is measured by comparing 
+            their hits on protein sequences: a sequence is considered <em>overlapped</em> 
+            when the regions matched by both signatures cover at least 80% 
+            of each other’s residues. The overall similarity between signatures is then derived 
+            from the proportion of shared overlapped sequences in Swiss-Prot and TrEMBL.
         </p>
-        <div class="ui warning message">
-            <i class="close icon"></i>
-            <div class="header">Checkboxes are read-only</div>
-            <p>To check or uncheck an entry, please visit the entry page.</p>
-        </div>
     `;
-
-    $(params.querySelectorAll('.message .close'))
-        .on('click', function() {
-            $(this)
-                .closest('.message')
-                .transition('fade');
-        });
-
-    for (let input of params.querySelectorAll('input')) {
-        input.addEventListener('change', e => {
-            const key = e.currentTarget.name;
-            const checked = e.currentTarget.checked;
-            const url = new URL(location.href, location.origin);
-
-            if (checked)
-                url.searchParams.set(key, '');
-            else if (url.searchParams.has(key))
-                url.searchParams.delete(key);
-
-            history.pushState(null, document.title, url.toString());
-            refresh();
-        });
-    }
 
     refresh();
 });
