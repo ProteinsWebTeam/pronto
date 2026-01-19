07dd1616203acb7b4e70b1c25e7b8f75842c2d42
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 <laisecflorentino@gmail.com>
2026-01-19 12:44:11 -0300

    Replace member database accessions with links in descriptions

--- pronto/api/entry/utils.py
+++ pronto/api/entry/utils.py
@@ -1,4 +1,20 @@
 import re
+from pronto.api.signatures import get_sig2interpro
+
+MEMBERS_PATTERNS = {
+    "cath-gene3d": r"\bG3DSA:\d+(?:\.\d+)+\b",
+    "cdd": r"\bcd\d+\b",
+    "hamap": r"\bMF_\d+\b",
+    "ncbifam": r"\bNF\d+\b",
+    "panther": r"\bPTHR\d+\b",
+    "pirsf": r"\bPIRSF\d+\b",
+    "pfam": r"\bPF\d+\b",
+    "prints": r"\bPR\d+\b",
+    "prosite": r"\bPS\d+\b",
+    "sfld": r"\bSFLD_\d+\b",
+    "smart": r"\bSM\d+\b",
+    "superfamily": r"\bSSF\d+\b"
+}
 
 
 def _replace_greek_letters(text):
@@ -25,10 +41,26 @@ def _replace_terminal(text):
                   lambda m: f"{m.group(1)}-terminal", text, flags=re.IGNORECASE)
 
 
+def _replace_accessions(text: str) -> str:
+    for member, pattern in MEMBERS_PATTERNS.items():
+        regex = re.compile(pattern, re.IGNORECASE)
+
+        def replacer(match):
+            accession = match.group(0)
+            ipr_acc = get_sig2interpro(accession)
+            if ipr_acc:
+                return f"[interpro:{ipr_acc}]"
+            return f"[{member}:{accession}]"
+
+        text = regex.sub(replacer, text)
+    return text
+
+
 def sanitize_description(text):
     if not text:
         return text
     text = _replace_greek_letters(text)
     text = _replace_terminus(text)
     text = _replace_terminal(text)
+    text = _replace_accessions(text)
     return text
