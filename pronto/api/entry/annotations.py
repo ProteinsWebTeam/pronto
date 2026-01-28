dfe58bf83ab30db137739e293ab889182796175c
apolignano <alessandro.polignano@ebi.ac.uk>
2026-01-28 16:35:15 +0100

    Inferred type and supp. ref in description

--- pronto/api/entry/annotations.py
+++ pronto/api/entry/annotations.py
@@ -113,6 +113,7 @@ def relate_entry_to_anno(
     ann_id: str,
     entry_acc: str,
     con: oracledb.Connection,
+    autointegration: bool = False
 ):
     """Link a newly inserted annotation to a new entry
 
@@ -149,7 +150,7 @@ def relate_entry_to_anno(
             """,
             [entry_acc, ann_id, order_in]
         )
-        update_references(cur, entry_acc)
+        update_references(cur, entry_acc, autointegration=autointegration)
     except DatabaseError as exc:
         return {
             "status": False,
@@ -380,7 +381,7 @@ def move_annotation(accession: str, ann_id: str, x: int):
     }), 200
 
 
-def update_references(cur: Cursor, accession: str):
+def update_references(cur: Cursor, accession: str, autointegration: bool = False):
     # Get references from the entry-pub table (may need to be updated)
     cur.execute(
         """
@@ -444,7 +445,7 @@ def update_references(cur: Cursor, accession: str):
             # Not in annotations: remove from DB
             old_references.append((accession, pub_id))
 
-    if old_references:
+    if old_references and not autointegration:
         # Move references to SUPPLEMENTARY_REF table
         cur.executemany(
             """
