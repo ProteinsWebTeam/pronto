3fbaa397aceeb7001fa7f144ea661c773e8ea3f7
Laise Florentino <laisecflorentino@gmail.com>
2026-01-29 19:47:08 +0000

    Update pronto/utils.py
    Co-authored-by: Matthias Blum <mat.blum@gmail.com>

--- pronto/utils.py
+++ pronto/utils.py
@@ -14,20 +14,20 @@ from flask import current_app
 
 
 SIGNATURES = {
-    r"G3DSA:[\d.]{4,}": "cath-gene3d",    # CATH-Gene3D
-    r"MF_\d{4,}": "hamap",                # HAMAP
-    r"PF\d{5,}": "pfam",                  # Pfam
-    r"PIRSF\d{4,}": "pirsf",              # PIRSF
-    r"PR\d{4,}": "prints",                # PROSITE
-    r"PS\d{4,}": "prosite",               # PROSITE
-    r"PTHR\d{4,}": "panther",             # PANTHER
-    r"SFLD[FGS]\d{4,}": "sfld",           # SFLD
-    r"SM\d{4,}": "smart",                 # SMART
-    r"SSF\d{4,}": "superfamily",          # SUPERFAMILY
-    r"NF\d{6}": "ncbifam",                # NCBIFAM
-    r"TIGR\d{4,}": "ncbifam",             # NCBIFAM
-    r"cd\d{4,}": "cdd",                   # CDD
-    r"sd\d{4,}": "cdd"                    # CDD
+    r"G3DSA:[\d.]{4,}": "cathgene3d",
+    r"MF_\d{4,}": "hamap",
+    r"PF\d{5,}": "pfam",
+    r"PIRSF\d{4,}": "pirsf",
+    r"PR\d{4,}": "prints",
+    r"PS\d{4,}": "prosite",
+    r"PTHR\d{4,}": "panther",
+    r"SFLD[FGS]\d{4,}": "sfld",
+    r"SM\d{4,}": "smart",
+    r"SSF\d{4,}": "superfamily",
+    r"NF\d{6}": "ncbifam",
+    r"TIGR\d{4,}": "ncbifam",
+    r"cd\d{4,}": "cdd",
+    r"sd\d{4,}": "cdd",
 }
 
 XREFS = {
