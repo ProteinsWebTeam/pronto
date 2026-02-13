212556d356538a6b5b92e3bbeea560e35ba6b74c
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 <laisecflorentino@gmail.com>
2026-02-13 14:34:48 +0000

    close conections

--- pronto/api/entry/utils.py
+++ pronto/api/entry/utils.py
@@ -92,8 +92,9 @@ def _standardise_citations(text: str) -> str:
 
 def _capitalize_first(text) -> str:
     con = connect_oracle()
-    cur = con.cursor()
-    keep_lower_terms = load_global_exceptions(cur, 'lower_case_name')
+    with con.cursor() as cur:
+        keep_lower_terms = load_global_exceptions(cur, 'lower_case_name')
+    con.close()
 
     for term in keep_lower_terms:
         if text.startswith(term):
